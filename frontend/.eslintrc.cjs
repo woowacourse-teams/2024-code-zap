f1ee5ee777f969bfb93764d8657f079d1206d84d
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-07-11 17:58:22 +0900

    chore: eslint 설정
    Co-Authored-By: MYONG JAEWI <78201530+Jaymyong66@users.noreply.github.com>
    Co-Authored-By: 월하 <52562061+vi-wolhwa@users.noreply.github.com>

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,15 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/jsx-runtime',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'react'],
+  rules: {},
+};
