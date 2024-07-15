8f0ce2f621a843cf1e4fbf1fa094101bba4c2e30
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-15 17:08:47 +0900

    feat(domain): JpaAuditing 기능 사용을 위한 설정 파일 및 BaseTimeEntity 생성
    Co-authored-by: hoeseong123 <ghltjd3@gmail.com>
    Co-authored-by: zangsu <zangsu_@naver.com>

---
+++ backend/src/main/java/codezap/global/domain/BaseTimeEntity.java
@@ -0,0 +1,24 @@
+package codezap.global.domain;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public class BaseTimeEntity {
+
+    @CreatedDate
+    @Column(updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime modifiedAt;
+}
