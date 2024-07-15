a7b373252dec80de93871e598631988c938d2cb7
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-15 17:26:48 +0900

    style(domain): Template 엔티티 코드 정렬
    Co-authored-by: hoeseong123 <ghltjd3@gmail.com>
    Co-authored-by: zangsu <zangsu_@naver.com>

--- backend/src/main/java/codezap/template/domain/Template.java
+++ backend/src/main/java/codezap/template/domain/Template.java
@@ -1,6 +1,13 @@
 package codezap.template.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
 
 import codezap.global.domain.BaseTimeEntity;
 import codezap.member.domain.Member;
