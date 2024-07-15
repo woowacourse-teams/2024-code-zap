b4ec85b630a5cd87647b4eec5337219afb071bf6
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-07-15 22:41:38 +0900

    feat: Template 페이지 - Title, code block 추가
    Co-Authored-By: MYONG JAEWI <78201530+Jaymyong66@users.noreply.github.com>

--- frontend/src/pages/Template.tsx
+++ frontend/src/pages/Template.tsx
@@ -1,5 +1,45 @@
+import { Flex } from '@/components/Flex';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { Text } from '@/components/Text';
+import mockTemplate from '../mocks/template.json';
+import { vscDarkPlus } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
 const Template = () => {
-  return <div>Template</div>;
+  return (
+    <>
+      <Flex
+        direction='column'
+        justify='center'
+        align='flex-start'
+        gap='1.5rem'
+        padding='10rem 0 0 0'
+      >
+        <Flex direction='column' justify='center' align='flex-start' gap='1rem'>
+          <Text.Title weight='bold'>{mockTemplate.title}</Text.Title>
+          <Text.Caption color='#FFD369'>
+            {mockTemplate.member.nickname}
+          </Text.Caption>
+        </Flex>
+
+        {mockTemplate.snippets.map((snippet) => {
+          return (
+            <SyntaxHighlighter
+              language='javascript'
+              style={vscDarkPlus}
+              showLineNumbers={true}
+              customStyle={{
+                borderRadius: '10px',
+                width: '70%',
+                tabSize: 2,
+              }}
+            >
+              {snippet.content}
+            </SyntaxHighlighter>
+          );
+        })}
+      </Flex>
+    </>
+  );
 };
 
 export default Template;
