0c6a12731612ee8c71d73d832129df7f201c985e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-15 20:02:25 +0900

    feat(domain): BaseTimeEntity 의 Getter 생성
    Co-authored-by: hoeseong123 <ghltjd3@gmail.com>
    Co-authored-by: zangsu <zangsu_@naver.com>

--- backend/src/main/java/codezap/global/domain/BaseTimeEntity.java
+++ backend/src/main/java/codezap/global/domain/BaseTimeEntity.java
@@ -10,8 +10,11 @@ import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import lombok.Getter;
+
 @EntityListeners(AuditingEntityListener.class)
 @MappedSuperclass
+@Getter
 public class BaseTimeEntity {
 
     @CreatedDate
