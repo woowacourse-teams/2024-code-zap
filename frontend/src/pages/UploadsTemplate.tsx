e486f9d7c690e8651204e360da8196a973a4074c
jayming66 <mjw8941@gmail.com>
2024-07-16 04:04:24 +0900

    feat(pages): UploadsTemplate의 컴포넌트 레이아웃

--- frontend/src/pages/UploadsTemplate.tsx
+++ frontend/src/pages/UploadsTemplate.tsx
@@ -1,5 +1,38 @@
+import { Button } from '@/components/Button';
+import { Flex } from '@/components/Flex';
+import { useState } from 'react';
+
 const UploadsTemplate = () => {
-  return <div>UploadsTemplate</div>;
+  const [snippets, setSnippets] = useState([
+    {
+      title: 'example.js',
+      content: 'const a = 1 + 2;',
+    },
+    {
+      title: 'module.js',
+      content: 'const a = 1 + 2;',
+    },
+  ]);
+
+  return (
+    <>
+      <Flex direction='column' justify='center' align='flex-start' gap='1.5rem' padding='10rem 0 0 0'>
+        <Flex direction='column' justify='center' align='flex-start' gap='1rem' width='100%'>
+          <input placeholder='템플릿명을 입력해주세요' style={{ width: '100%' }}></input>
+
+          {snippets.map((snippet, idx) => {
+            return <textarea key={idx} style={{ width: '100%', height: '10rem' }}></textarea>;
+          })}
+          <Flex direction='row' justify='space-between' width='100%'>
+            <Button width='auto' type='outlined'>
+              + Add Snippet
+            </Button>
+            <Button>Save</Button>
+          </Flex>
+        </Flex>
+      </Flex>
+    </>
+  );
 };
 
 export default UploadsTemplate;
