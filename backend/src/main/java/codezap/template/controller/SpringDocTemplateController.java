f70d45e5797a27bc8ff6480f0b6ee324f268b305
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-15 15:44:48 +0900

    docs: 템플릿 단건 조회 API 작성

--- backend/src/main/java/codezap/template/controller/SpringDocTemplateController.java
+++ backend/src/main/java/codezap/template/controller/SpringDocTemplateController.java
@@ -1,10 +1,12 @@
 package codezap.template.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import codezap.template.dto.request.CreateTemplateResponse;
 import codezap.template.dto.response.CreateTemplateRequest;
 import codezap.template.dto.response.FindAllTemplatesResponse;
+import codezap.template.dto.response.FindTemplateByIdResponse;
 import io.swagger.v3.oas.annotations.Operation;
 
 public interface SpringDocTemplateController {
@@ -21,5 +23,7 @@ public interface SpringDocTemplateController {
     @Operation(summary = "템플릿 목록 조회", description = "작성된 모든 템플릿을 조회합니다.")
     ResponseEntity<FindAllTemplatesResponse> getTemplates();
 
+    @Operation(summary = "템플릿 단건 조회", description = "해당하는 식별자의 템플릿을 조회합니다.")
+    ResponseEntity<FindTemplateByIdResponse> getTemplateById(@PathVariable Long id);
 }
 
--- backend/src/main/java/codezap/template/controller/TemplateController.java
+++ backend/src/main/java/codezap/template/controller/TemplateController.java
@@ -3,6 +3,7 @@ package codezap.member.controller;
 import org.apache.commons.lang3.NotImplementedException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +13,7 @@ import codezap.template.controller.SpringDocTemplateController;
 import codezap.template.dto.request.CreateTemplateResponse;
 import codezap.template.dto.response.CreateTemplateRequest;
 import codezap.template.dto.response.FindAllTemplatesResponse;
+import codezap.template.dto.response.FindTemplateByIdResponse;
 
 @RestController
 @RequestMapping("/templates")
@@ -24,6 +26,11 @@ public class TemplateController implements SpringDocTemplateController {
 
     @GetMapping("")
     public ResponseEntity<FindAllTemplatesResponse> getTemplates() {
+        throw new NotImplementedException();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<FindTemplateByIdResponse> getTemplateById(@PathVariable Long id) {
         throw new NotImplementedException();
     }
 }
