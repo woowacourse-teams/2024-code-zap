b2b617b65a3a514af5ec1bc67e83fe68e70aa11c
jayming66 <mjw8941@gmail.com>
2024-07-16 10:51:30 +0900

    refactor(TemplateTitleInput): TemplateTitleInput 컴포넌트 분리

--- frontend/src/pages/UploadsTemplate.tsx
+++ frontend/src/pages/UploadsTemplate.tsx
@@ -1,10 +1,9 @@
 import { useCallback, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { css } from '@emotion/react';
-import styled from '@emotion/styled';
 import { Button } from '@/components/Button';
 import { Flex } from '@/components/Flex';
 import { SnippetEditor } from '@/components/SnippetEditor';
+import { TemplateTitleInput } from '@/components/TemplateTitleInput';
 
 const UploadsTemplate = () => {
   const navigate = useNavigate();
@@ -39,10 +38,11 @@ const UploadsTemplate = () => {
     <>
       <Flex direction='column' justify='center' align='flex-start' gap='1.5rem' padding='10rem 0'>
         <Flex direction='column' justify='center' align='flex-start' gap='1rem' width='100%'>
-          <Input placeholder='템플릿명을 입력해주세요'></Input>
+          <TemplateTitleInput placeholder='템플릿명을 입력해주세요' />
           {snippets.map((snippet, idx) => {
             return <SnippetEditor key={idx} content={snippet.content} onChange={(val) => handleCodeChange(val, idx)} />;
           })}
+
           <Flex direction='row' justify='space-between' width='100%' padding='3rem 0 0 0'>
             <Button width='auto' type='outlined' onClick={handleAddButtonClick}>
               + Add Snippet
@@ -56,40 +56,3 @@ const UploadsTemplate = () => {
 };
 
 export default UploadsTemplate;
-
-const inputStyles = css`
-  width: 100%;
-  padding: 10px 0;
-  background: none;
-  border: none;
-  border-bottom: 1px solid #555555;
-  color: #cccccc;
-  font-size: 16px;
-
-  &::placeholder {
-    color: #808080;
-  }
-
-  &:focus {
-    outline: none;
-    border-bottom: 1px solid #cccccc;
-  }
-`;
-
-const InputWrapper = styled.div`
-  position: relative;
-  margin: 20px 0;
-  width: 100%;
-`;
-
-const StyledInput = styled.input`
-  ${inputStyles}
-`;
-
-const Input = ({ placeholder }: { placeholder: string }) => {
-  return (
-    <InputWrapper>
-      <StyledInput placeholder={placeholder} />
-    </InputWrapper>
-  );
-};
