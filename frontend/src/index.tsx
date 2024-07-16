bf22f6d0f9d7628eee80382593dcf13a0eb6ac32
월하 <cerezo00@naver.com>
2024-07-16 12:45:18 +0900

    feat(src): index.tsx QueryClient 세팅

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -2,14 +2,19 @@ import GlobalStyles from './style/GlobalStyles';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import router from './routes/router';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <RouterProvider router={router} />
+    <QueryClientProvider client={ queryClient }>
+      <GlobalStyles />
+      <RouterProvider router={ router } />
+    </QueryClientProvider>
   </React.StrictMode>
 );
