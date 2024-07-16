38ecd2b4bdd1fba3395773de4f40e8bad9474c80
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-16 09:35:21 +0900

    feat(domain): RepresentativeSnippet 엔티티 pk, fk 분리
    Co-authored-by: hoeseong123 <ghltjd3@gmail.com>
    Co-authored-by: zangsu <zangsu_@naver.com>

--- backend/src/main/java/codezap/representative_snippet/domain/RepresentativeSnippet.java
+++ backend/src/main/java/codezap/representative_snippet/domain/RepresentativeSnippet.java
@@ -1,26 +1,31 @@
 package codezap.representative_snippet.domain;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 import codezap.global.domain.BaseTimeEntity;
 import codezap.snippet.domain.Snippet;
 import codezap.template.domain.Template;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "representative_snippet")
 @Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class RepresentativeSnippet extends BaseTimeEntity {
 
     @Id
-    private Long templateId;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
-    @MapsId
     @OneToOne
     @JoinColumn(name = "template_id")
     private Template template;
