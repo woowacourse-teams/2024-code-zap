9e280b65f52f38740cb7097816167f36762b369e
월하 <cerezo00@naver.com>
2024-07-21 23:14:57 +0900

    refactor(Flex): style props 객체분해할당

--- frontend/src/components/Flex/style.ts
+++ frontend/src/components/Flex/style.ts
@@ -9,15 +9,15 @@ interface StyleProps
 
 export const FlexContainer = styled.div<StyleProps>`
   display: flex;
-  flex: ${(props) => props.flex};
-  flex-flow: ${(props) => props.direction};
-  flex-wrap: ${(props) => props.wrap};
+  flex: ${({ flex }) => flex};
+  flex-flow: ${({ direction }) => direction};
+  flex-wrap: ${({ wrap }) => wrap};
   gap: ${(props) => props.gap};
-  align-items: ${(props) => props.align};
-  justify-content: ${(props) => props.justify};
+  align-items: ${({ align }) => align};
+  justify-content: ${({ justify }) => justify};
 
-  width: ${(props) => props.width};
-  height: ${(props) => props.height};
-  margin: ${(props) => props.margin};
-  padding: ${(props) => props.padding};
+  width: ${({ width }) => width};
+  height: ${({ height }) => height};
+  margin: ${({ margin }) => margin};
+  padding: ${({ padding }) => padding};
 `;
