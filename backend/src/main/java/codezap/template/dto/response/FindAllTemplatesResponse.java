ce606352c21c73be42995a0465b14462bbaa8f40
zangsu <zangsu_@naver.com>
2024-07-22 17:49:57 +0900

    refactor(response): DTO 이름을 직관적으로 변경 및 내부 클래스로 이동
    Co-authored-by: hoeseong123 <ghltjd3@gmail.com>

--- backend/src/main/java/codezap/template/dto/response/FindAllTemplatesResponse.java
+++ backend/src/main/java/codezap/template/dto/response/FindAllTemplatesResponse.java
@@ -1,16 +1,33 @@
 package codezap.template.dto.response;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import codezap.template.domain.ThumbnailSnippet;
 
 public record FindAllTemplatesResponse(
-        List<FindTemplateBySummaryResponse> templates
+        List<ItemResponse> templates
 ) {
     public static FindAllTemplatesResponse from(List<ThumbnailSnippet> thumbnailSnippets) {
-        List<FindTemplateBySummaryResponse> templatesBySummaryResponse = thumbnailSnippets.stream()
-                .map(FindTemplateBySummaryResponse::from)
+        List<ItemResponse> templatesBySummaryResponse = thumbnailSnippets.stream()
+                .map(ItemResponse::from)
                 .toList();
         return new FindAllTemplatesResponse(templatesBySummaryResponse);
+    }
+
+    public record ItemResponse(
+            Long id,
+            String title,
+            FindThumbnailSnippetResponse thumbnailSnippet,
+            LocalDateTime modifiedAt
+    ) {
+        public static ItemResponse from(ThumbnailSnippet thumbnailSnippet) {
+            return new ItemResponse(
+                    thumbnailSnippet.getTemplate().getId(),
+                    thumbnailSnippet.getTemplate().getTitle(),
+                    FindThumbnailSnippetResponse.from(thumbnailSnippet.getSnippet()),
+                    thumbnailSnippet.getModifiedAt()
+            );
+        }
     }
 }
