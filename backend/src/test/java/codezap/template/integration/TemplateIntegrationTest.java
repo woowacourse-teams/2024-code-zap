1d61b330cf10883faa50d094b979188f14749f21
zangsu <zangsu_@naver.com>
2024-07-22 15:44:11 +0900

    test(integration): Template 목록 조회 및 상세 조회 테스트 코드 추가
    Co-authored-by: hoeseong123 <ghltjd3@gmail.com>

--- backend/src/test/java/codezap/template/integration/TemplateIntegrationTest.java
+++ backend/src/test/java/codezap/template/integration/TemplateIntegrationTest.java
@@ -1,4 +1,4 @@
-package codezap.template;
+package codezap.template.integration;
 
 import static org.hamcrest.Matchers.is;
 
@@ -10,6 +10,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
 
 import codezap.template.dto.request.CreateSnippetRequest;
 import codezap.template.dto.request.CreateTemplateRequest;
@@ -17,6 +19,8 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@Sql(value = "/clear.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clear.sql", executionPhase = ExecutionPhase.AFTER_TEST_CLASS)
 class TemplateIntegrationTest {
 
     @LocalServerPort
@@ -67,5 +71,53 @@ class TemplateIntegrationTest {
                 .then().log().all()
                 .statusCode(400)
                 .body("detail", is("파일 이름은 최대 255자까지 입력 가능합니다."));
+    }
+    @Test
+    @DisplayName("템플릿 전체 조회 성공")
+    void findAllTemplatesSuccess() {
+        //given
+        CreateTemplateRequest templateRequest1 = new CreateTemplateRequest("title1",
+                List.of(new CreateSnippetRequest("filename", "content", 1)));
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(templateRequest1)
+                .when().post("/templates")
+                .then().log().all();
+
+        CreateTemplateRequest templateRequest2 = new CreateTemplateRequest("title2",
+                List.of(new CreateSnippetRequest("filename", "content", 1)));
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(templateRequest2)
+                .when().post("/templates")
+                .then().log().all();
+
+        //when
+        RestAssured.given().log().all()
+                .get("/templates")
+                .then().log().all()
+                .statusCode(200)
+                .body("templates.size()", is(2));
+    }
+
+    @Test
+    @DisplayName("템플릿 상세 조회 성공")
+    void findOneTemplateSuccess() {
+        //given
+        CreateTemplateRequest templateRequest = new CreateTemplateRequest("title",
+                List.of(new CreateSnippetRequest("filename", "content", 1)));
+        RestAssured.given().log().all()
+                .contentType(ContentType.JSON)
+                .body(templateRequest)
+                .when().post("/templates")
+                .then().log().all();
+
+        //when
+        RestAssured.given().log().all()
+                .get("/templates/1")
+                .then().log().all()
+                .statusCode(200)
+                .body("title", is(templateRequest.title()),
+                        "snippets.size()", is(1));
     }
 }
