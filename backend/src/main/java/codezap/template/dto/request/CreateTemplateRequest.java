fda72f676e3e9c8c3edafd2e861e49f9d6c76356
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-23 17:44:18 +0900

    docs: request 객체 설명 추가
    Co-authored-by: kyum-q <kyungmi.dev@gmail.com>

--- backend/src/main/java/codezap/template/dto/request/CreateTemplateRequest.java
+++ backend/src/main/java/codezap/template/dto/request/CreateTemplateRequest.java
@@ -7,11 +7,15 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import codezap.template.dto.request.validation.IncreasedIndex;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public record CreateTemplateRequest(
+        @Schema(description = "템플릿 이름", example = "스프링 로그인 구현")
         @NotNull(message = "템플릿 이름이 null 입니다.")
         @Size(max = 255, message = "템플릿 이름은 최대 255자까지 입력 가능합니다.")
         String title,
+
+        @Schema(description = "템플릿의 스니펫 내역")
         @NotNull(message = "스니펫 리스트가 null 입니다.")
         @IncreasedIndex(message = "스니펫 순서가 잘못되었습니다.")
         @Valid
