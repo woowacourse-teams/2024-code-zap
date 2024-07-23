681f58c1576067b21b62799eb59c767f883aba4d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-23 17:37:47 +0900

    docs: 템플릿 생성, 목록 및 단건 조회 API의 실패 응답 문서화
    Co-authored-by: kyum-q <kyungmi.dev@gmail.com>

--- backend/src/main/java/codezap/template/controller/SpringDocTemplateController.java
+++ backend/src/main/java/codezap/template/controller/SpringDocTemplateController.java
@@ -1,5 +1,6 @@
 package codezap.template.controller;
 
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -7,22 +8,44 @@ import codezap.template.dto.request.CreateTemplateRequest;
 import codezap.template.dto.response.FindAllTemplatesResponse;
 import codezap.template.dto.response.FindTemplateByIdResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.ExampleObject;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 
 public interface SpringDocTemplateController {
 
     @Operation(summary = "템플릿 생성", description = """
-            새로운 템플릿을 생성합니다. \n 
+            새로운 템플릿을 생성합니다. \n
             템플릿의 제목, 썸네일 스니펫의 순서, 스니펫 목록이 필요합니다. \n
             스니펫 목록은 파일 이름, 소스 코드, 해당 스니펫의 순서가 필요합니다. \n
             * 썸네일 스니펫은 1로 고정입니다. (2024.07.15 기준) \n
             * 모든 스니펫 순서는 1부터 시작합니다. \n
             """)
+    @ApiResponse(responseCode = "201", description = "회원 예약 생성 성공", headers = {
+            @Header(name = "생성된 템플릿의 API 경로", example = "/templates/1")})
+    @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ProblemDetail.class),
+            examples = {
+                    @ExampleObject(summary = "모든 필드 중 null인 값이 있는 경우",
+                            name = "메시지 예시: 템플릿 이름이 null 입니다."),
+                    @ExampleObject(summary = "제목 또는 스니펫 파일명이 255자를 초과한 경우",
+                            name = "메시지 예시: 제목은 최대 255자까지 입력 가능합니다."),
+                    @ExampleObject(summary = "썸네일 스니펫의 순서가 1이 아닌 경우",
+                            name = "메시지 예시: 식별자 1에 해당하는 템플릿이 존재하지 않습니다."),
+                    @ExampleObject(summary = "스니펫 내용 65,535 byte를 초과한 경우",
+                            name = "메시지 예시: 파일 내용은 최대 65,535 byte까지 입력 가능합니다.")}))
     ResponseEntity<Void> create(CreateTemplateRequest createTemplateRequest);
 
     @Operation(summary = "템플릿 목록 조회", description = "작성된 모든 템플릿을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "조회 성공",
+            content = {@Content(schema = @Schema(implementation = FindAllTemplatesResponse.class))})
     ResponseEntity<FindAllTemplatesResponse> getTemplates();
 
     @Operation(summary = "템플릿 단건 조회", description = "해당하는 식별자의 템플릿을 조회합니다.")
+    @ApiResponse(responseCode = "200", description = "템플릿 단건 조회 성공",
+            content = {@Content(schema = @Schema(implementation = FindAllTemplatesResponse.class))})
+    @ApiResponse(responseCode = "400", description = "해당하는 id 값인 템플릿이 없는 경우",
+            content = {@Content(schema = @Schema(implementation = ProblemDetail.class))})
     ResponseEntity<FindTemplateByIdResponse> getTemplateById(@PathVariable Long id);
 }
-
