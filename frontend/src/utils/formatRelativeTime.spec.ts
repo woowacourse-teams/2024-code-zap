1fc5aa24be95b7967dd46f6d12bc606759d46c1f
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-07-23 22:30:07 +0900

    test(utils): formatRelativeTime

---
+++ frontend/src/utils/formatRelativeTime.spec.ts
@@ -0,0 +1,28 @@
+import { formatRelativeTime } from './formatRelativeTime';
+
+describe('formatRelativeTime', () => {
+  test('date가 현재시간과 비교하여 10분 이내 차이가 나는 경우 `방금전`을 리턴한다.', () => {
+    const now = new Date('2024-01-01T12:00:00');
+    const date = new Date('2024-01-01T11:55:00');
+
+    expect(formatRelativeTime(date.toISOString(), now)).toBe('방금 전');
+  });
+  test('date가 현재시간과 비교하여 1시간 이내 차이가 나는 경우 `몇 분 전`을 리턴한다.', () => {
+    const now = new Date('2024-01-01T12:00:00');
+    const date = new Date('2024-01-01T11:30:00');
+
+    expect(formatRelativeTime(date.toISOString(), now)).toBe('30분 전');
+  });
+  test('date가 현재시간과 비교하여 24시간 이내 차이가 나는 경우 `몇 시간 전`을 리턴한다.', () => {
+    const now = new Date('2024-01-01T12:00:00');
+    const date = new Date('2024-01-01T10:00:00');
+
+    expect(formatRelativeTime(date.toISOString(), now)).toBe('2시간 전');
+  });
+  test('date가 현재시간과 비교하여 24시간 이상 차이가 나는 경우 `몇 년 몇 월 며칠`을 리턴한다.', () => {
+    const now = new Date('2024-01-03T12:00:00');
+    const date = new Date('2024-01-01T12:00:00');
+
+    expect(formatRelativeTime(date.toISOString(), now)).toBe('2024년 1월 1일');
+  });
+});
