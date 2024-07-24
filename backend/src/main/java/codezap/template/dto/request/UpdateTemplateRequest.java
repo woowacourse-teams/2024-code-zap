6c83831d0c373c72fb652e646d2eb6c048989d6d
HoeSeong123 <ghltjd3@gmail.com>
2024-07-24 15:55:33 +0900

    refactor(request): 제약 조건 추가
    Co-authored-by: zangsu <zangsu_@naver.com>

--- backend/src/main/java/codezap/template/dto/request/UpdateTemplateRequest.java
+++ backend/src/main/java/codezap/template/dto/request/UpdateTemplateRequest.java
@@ -2,19 +2,25 @@ package codezap.template.dto.request;
 
 import java.util.List;
 
+import jakarta.validation.constraints.NotNull;
+
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record UpdateTemplateRequest(
         @Schema(description = "템플릿 이름", example = "스프링 로그인 구현")
+        @NotNull(message = "템플릿 이름이 null 입니다.")
         String title,
 
         @Schema(description = "새로 추가한 스니펫 내역")
+        @NotNull(message = "createSnippets 리스트가 null 입니다.")
         List<CreateSnippetRequest> createSnippets,
 
         @Schema(description = "삭제, 생성 스니펫을 제외한 모든 스니펫 내역")
+        @NotNull(message = "updateSnippets 리스트가 null 입니다.")
         List<UpdateSnippetRequest> updateSnippets,
 
         @Schema(description = "삭제한 스니펫 식별자")
+        @NotNull(message = "deleteSnippetIds 리스트가 null 입니다.")
         List<Long> deleteSnippetIds
 ) {
 }
