04ded6ea704cbbc40c4af871d85c5b2fa8979f22
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-07-24 16:33:35 +0900

    feat(routes): basename 설정

--- frontend/src/routes/router.tsx
+++ frontend/src/routes/router.tsx
@@ -4,24 +4,27 @@ import Template from '@/pages/Template';
 import TemplateList from '@/pages/TemplateList';
 import TemplateUpload from '@/pages/TemplateUpload';
 
-const router = createBrowserRouter([
-  {
-    element: <Layout />,
-    children: [
-      {
-        path: '/',
-        element: <TemplateList />,
-      },
-      {
-        path: 'templates/:id',
-        element: <Template />,
-      },
-      {
-        path: 'templates/uploads',
-        element: <TemplateUpload />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  [
+    {
+      element: <Layout />,
+      children: [
+        {
+          path: '/',
+          element: <TemplateList />,
+        },
+        {
+          path: 'templates/:id',
+          element: <Template />,
+        },
+        {
+          path: 'templates/uploads',
+          element: <TemplateUpload />,
+        },
+      ],
+    },
+  ],
+  { basename: process.env.NODE_ENV === 'development' ? '/' : process.env.REACT_APP_BASE_URL },
+);
 
 export default router;
