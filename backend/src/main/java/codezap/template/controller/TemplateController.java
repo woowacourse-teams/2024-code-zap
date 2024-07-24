aece2caf640f8b99da4dc0cb19decf7bc7e31c10
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-07-24 18:18:58 +0900

    feat(controller): 템플릿 토픽 검색 성공

--- backend/src/main/java/codezap/template/controller/TemplateController.java
+++ backend/src/main/java/codezap/template/controller/TemplateController.java
@@ -3,7 +3,6 @@ package codezap.template.controller;
 import java.net.URI;
 
 import org.apache.commons.lang3.NotImplementedException;
-import org.springframework.data.repository.query.Param;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,6 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import codezap.global.validation.ValidationSequence;
@@ -50,8 +50,8 @@ public class TemplateController implements SpringDocTemplateController {
     }
 
     @GetMapping("/search")
-    public ResponseEntity<FindAllTemplatesResponse> getTemplatesContainTopic(@Param("topic") String topic) {
-        throw new NotImplementedException();
+    public ResponseEntity<FindAllTemplatesResponse> getTemplatesContainTopic(@RequestParam String topic) {
+        return ResponseEntity.ok(templateService.findContainTopic(topic));
     }
 
     @PostMapping("/{id}")
