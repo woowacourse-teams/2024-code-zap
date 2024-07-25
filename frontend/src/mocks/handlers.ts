050425a8922ce6833f9761e8da1be342c939f53c
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-07-25 13:47:16 +0900

    feat(mocks): template 수정, 삭제 handlers 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -15,4 +15,6 @@ export const handlers = [
     return response;
   }),
   http.post(`${TEMPLATE_API_URL}`, async () => HttpResponse.json({ status: 201 })),
+  http.post(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 200 })),
+  http.delete(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 204 })),
 ];
