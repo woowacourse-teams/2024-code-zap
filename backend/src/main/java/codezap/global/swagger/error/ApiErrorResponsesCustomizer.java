4a9b33ec35d6565dbdab5996d213a5d208d6e9dd
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-07-26 01:26:23 +0900

    refactor(swagger): 커스텀된 에러 응답을 Swagger 문서에 자동으로 추가하는 커스터마이저 구현

---
+++ backend/src/main/java/codezap/global/swagger/error/ApiErrorResponsesCustomizer.java
@@ -0,0 +1,47 @@
+package codezap.global.swagger.error;
+
+import java.util.Arrays;
+
+import org.springdoc.core.customizers.OperationCustomizer;
+import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
+
+import io.swagger.v3.oas.models.Operation;
+import io.swagger.v3.oas.models.examples.Example;
+import io.swagger.v3.oas.models.media.Content;
+import io.swagger.v3.oas.models.media.MediaType;
+import io.swagger.v3.oas.models.responses.ApiResponse;
+import io.swagger.v3.oas.models.responses.ApiResponses;
+
+@Component
+public class ApiErrorResponsesCustomizer implements OperationCustomizer {
+
+    @Override
+    public Operation customize(Operation operation, HandlerMethod handlerMethod) {
+        if (handlerMethod.hasMethodAnnotation(ApiErrorResponse.class)) {
+            ApiErrorResponse apiErrorResponse = handlerMethod.getMethodAnnotation(ApiErrorResponse.class);
+            ApiResponses responses = operation.getResponses();
+            responses.addApiResponse(apiErrorResponse.status().name(), makeFailResponse(apiErrorResponse));
+        }
+        return operation;
+    }
+
+    private ApiResponse makeFailResponse(ApiErrorResponse apiErrorResponse) {
+        ApiResponse apiResponse = new ApiResponse().description(apiErrorResponse.status().name());
+        return apiResponse.content(new Content().addMediaType("application/json", makeMediaType(apiErrorResponse)));
+    }
+
+    private MediaType makeMediaType(ApiErrorResponse apiErrorResponse) {
+        ErrorCase[] errorCases = apiErrorResponse.failCases();
+
+        MediaType mediaType = new MediaType();
+        Arrays.stream(errorCases).forEach(
+                errorCase -> mediaType.addExamples(errorCase.description(), makeExample(apiErrorResponse, errorCase)));
+        return mediaType;
+    }
+
+    private Example makeExample(ApiErrorResponse apiErrorResponse, ErrorCase failResponse) {
+        Example example = new Example().summary(failResponse.description());
+        return example.value(ProblemDetailSchema.of(apiErrorResponse, failResponse.exampleMessage()));
+    }
+}
