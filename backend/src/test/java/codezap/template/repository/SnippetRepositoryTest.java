8b6da7680b1f8e5ce3f10eb11f996a18fbac1df2
HoeSeong123 <ghltjd3@gmail.com>
2024-07-25 13:46:44 +0900

    test(controller): 복잡한 쿼리 메서드 테스트 추가
    Co-authored-by: zangsu <zangsu_@naver.com>

---
+++ backend/src/test/java/codezap/template/repository/SnippetRepositoryTest.java
@@ -0,0 +1,66 @@
+package codezap.template.repository;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertAll;
+
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.Sql.ExecutionPhase;
+import org.springframework.transaction.annotation.Transactional;
+
+import codezap.template.domain.Snippet;
+import codezap.template.domain.Template;
+
+@SpringBootTest
+@Sql(value = "/clear.sql", executionPhase = ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = "/clear.sql", executionPhase = ExecutionPhase.AFTER_TEST_CLASS)
+@Transactional
+class SnippetRepositoryTest {
+
+    @Autowired
+    private SnippetRepository snippetRepository;
+    @Autowired
+    private TemplateRepository templateRepository;
+
+    @Test
+    @DisplayName("단일 스니펫 찾기 성공: 템플릿과 순서")
+    void findOneSnippetSuccessWithTemplateAndOrdinal() {
+        Template template = templateRepository.save(new Template("title"));
+        Snippet snippet1 = snippetRepository.save(new Snippet(template, "filename1", "content1", 1));
+        Snippet snippet2 = snippetRepository.save(new Snippet(template, "filename2", "content2", 2));
+
+        Snippet foundSnippet = snippetRepository.findByTemplateAndOrdinal(template, 2);
+
+        assertAll(
+                () -> assertThat(foundSnippet.getTemplate().getTitle()).isEqualTo(template.getTitle()),
+                () -> assertThat(foundSnippet.getFilename()).isEqualTo(snippet2.getFilename()),
+                () -> assertThat(foundSnippet.getContent()).isEqualTo(snippet2.getContent()),
+                () -> assertThat(foundSnippet.getOrdinal()).isEqualTo(snippet2.getOrdinal())
+        );
+    }
+
+    @Test
+    @DisplayName("스니펫 리스트 찾기 성공: 템플릿과 순서")
+    void findSnippetsSuccessWithTemplateAndOrdinal() {
+        Template template = templateRepository.save(new Template("title"));
+        Snippet snippet1 = snippetRepository.save(new Snippet(template, "filename1", "content1", 1));
+        Snippet snippet2 = snippetRepository.save(new Snippet(template, "filename2", "content2", 2));
+        Snippet snippet3 = snippetRepository.save(new Snippet(template, "filename3", "content3", 2));
+
+        List<Snippet> foundSnippets = snippetRepository.findAllByTemplateAndOrdinal(template, 2);
+
+        assertAll(
+                () -> assertThat(foundSnippets).hasSize(2),
+                () -> assertThat(foundSnippets).allMatch(snippet -> snippet.getTemplate().getId().equals(1L)),
+                () -> assertThat(foundSnippets).anyMatch(
+                        snippet -> snippet.getFilename().equals(snippet2.getFilename())),
+                () -> assertThat(foundSnippets).anyMatch(
+                        snippet -> snippet.getFilename().equals(snippet3.getFilename()))
+        );
+    }
+}
