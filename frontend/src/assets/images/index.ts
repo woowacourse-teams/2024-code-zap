91f5e204d55e99740b6b73dc7072d6ae1ebf038b
jayming66 <mjw8941@gmail.com>
2024-07-25 21:19:28 +0900

    feat(assets): 휴지통 아이콘 추가

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -1,3 +1,4 @@
 export { default as logoIcon } from './logo.png';
 export { default as newTemplateIcon } from './newTemplate.png';
 export { default as searchIcon } from './search.png';
+export { default as trashcanIcon } from './trashcan.png';
