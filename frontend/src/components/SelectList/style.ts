00d402ff92cb9386e3b9a97121584c3589d71af4
월하 <cerezo00@naver.com>
2024-07-26 11:03:18 +0900

    refactor(SelectList): 'SelectList' 디자인 변경

--- frontend/src/components/SelectList/style.ts
+++ frontend/src/components/SelectList/style.ts
@@ -2,15 +2,55 @@ import styled from '@emotion/styled';
 
 import type { OptionProps } from './SelectList';
 
+export const SelectListContainer = styled.div`
+  position: relative;
+
+  display: flex;
+  flex-direction: column;
+  gap: 0.6rem;
+
+  border-radius: 0.8rem;
+
+  &:hover {
+    gap: 0;
+    padding: 1.2rem;
+    background: #fff;
+  }
+
+  &:hover a {
+    width: 24rem;
+    height: 3.2rem;
+    padding: 0 0.8rem;
+    border-radius: 0.8rem;
+  }
+
+  &:hover .select-list-text {
+    margin-left: 1rem;
+    opacity: 1;
+    transition: all 0.3s ease-in-out;
+  }
+`;
+
 export const SelectListOption = styled.a<OptionProps>`
   display: flex;
   align-items: center;
 
-  width: 24rem;
-  padding: 1rem 1.6rem;
+  width: 1rem;
+  height: 1rem;
 
   text-decoration: none;
 
-  background-color: ${({ isSelected }) => isSelected && '#FFEBBB'};
-  border-radius: 8px;
+  background-color: ${({ isSelected }) => (isSelected ? '#FFEBBB' : '#fff')};
+  border-radius: 50%;
+
+  transition: all 0.2s ease-in-out;
+
+  &:hover {
+    background: ${({ isSelected }) => isSelected || '#eee'};
+  }
+`;
+
+export const SelectListText = styled.div`
+  opacity: 0;
+  transition: all 0.1s ease-in-out;
 `;
