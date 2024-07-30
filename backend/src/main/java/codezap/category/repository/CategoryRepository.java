778f92a2cc2f4e67bd359c836e29ba959017aaa3
HoeSeong123 <ghltjd3@gmail.com>
2024-07-30 21:29:18 +0900

    feat(category): category 이름 중복 검증 기능 구현

--- backend/src/main/java/codezap/category/repository/CategoryRepository.java
+++ backend/src/main/java/codezap/category/repository/CategoryRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import codezap.category.domain.Category;
 
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    boolean existsByName(String name);
 }
