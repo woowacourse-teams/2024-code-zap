de7d1ae848a084ca79a30e542ef5a72bd63c98a6
HoeSeong123 <ghltjd3@gmail.com>
2024-07-30 20:46:04 +0900

    refactor(domain): Tag와 Category에 기본 키 생성 어노테이션 추가
    Co-authored-by: zangsu <zangsu_@naver.com>

--- backend/src/main/java/codezap/template/domain/Tag.java
+++ backend/src/main/java/codezap/template/domain/Tag.java
@@ -2,6 +2,8 @@ package codezap.template.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 import codezap.global.auditing.BaseTimeEntity;
@@ -14,6 +16,7 @@ import lombok.NoArgsConstructor;
 public class Tag extends BaseTimeEntity {
 
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
