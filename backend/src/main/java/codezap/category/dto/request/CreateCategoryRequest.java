6b99ee7828411c0533daa2b7f3af9149983ff8ae
HoeSeong123 <ghltjd3@gmail.com>
2024-07-30 21:03:23 +0900

    feat(category): category 이름 검증 기능 구현

--- backend/src/main/java/codezap/category/dto/request/CreateCategoryRequest.java
+++ backend/src/main/java/codezap/category/dto/request/CreateCategoryRequest.java
@@ -1,6 +1,11 @@
 package codezap.category.dto.request;
 
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
 public record CreateCategoryRequest(
+        @NotNull(message = "카테고리 이름이 null 입니다.")
+        @Size(max = 255, message = "카테고리 이름은 최대 255자까지 입력 가능합니다.")
         String name
 ) {
 }
--- backend/src/test/java/codezap/category/controller/CategoryControllerTest.java
+++ backend/src/test/java/codezap/category/controller/CategoryControllerTest.java
@@ -1,7 +1,10 @@
 package codezap.category.controller;
 
+import static org.hamcrest.Matchers.is;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -18,6 +21,8 @@ import io.restassured.http.ContentType;
 @Sql(value = "/clear.sql", executionPhase = ExecutionPhase.AFTER_TEST_CLASS)
 class CategoryControllerTest {
 
+    private static final int MAX_LENGTH = 255;
+
     @LocalServerPort
     int port;
 
@@ -26,17 +31,35 @@ class CategoryControllerTest {
         RestAssured.port = port;
     }
 
-    @Test
-    @DisplayName("카테고리 생성 성공")
-    void createCategorySuccess() {
-        CreateCategoryRequest createCategoryRequest = new CreateCategoryRequest("category");
+    @Nested
+    @DisplayName("카테고리 생성 테스트")
+    class createCategoryTest {
+        @Test
+        @DisplayName("카테고리 생성 성공")
+        void createCategorySuccess() {
+            CreateCategoryRequest createCategoryRequest = new CreateCategoryRequest("a".repeat(MAX_LENGTH));
 
-        RestAssured.given().log().all()
-                .contentType(ContentType.JSON)
-                .body(createCategoryRequest)
-                .when().post("/categories")
-                .then().log().all()
-                .header("Location", "/categories/1")
-                .statusCode(201);
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(createCategoryRequest)
+                    .when().post("/categories")
+                    .then().log().all()
+                    .header("Location", "/categories/1")
+                    .statusCode(201);
+        }
+
+        @Test
+        @DisplayName("카테고리 생성 실패:")
+        void createCategoryFail() {
+            CreateCategoryRequest createCategoryRequest = new CreateCategoryRequest("a".repeat(MAX_LENGTH + 1));
+
+            RestAssured.given().log().all()
+                    .contentType(ContentType.JSON)
+                    .body(createCategoryRequest)
+                    .when().post("/categories")
+                    .then().log().all()
+                    .statusCode(400)
+                    .body("detail", is("카테고리 이름은 최대 255자까지 입력 가능합니다."));
+        }
     }
 }
