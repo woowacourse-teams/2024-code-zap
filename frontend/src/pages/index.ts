39f4cdcc49bb1cc9a7ed508069d51a19b50dd242
jayming66 <mjw8941@gmail.com>
2024-07-31 18:03:11 +0900

    feat(SignupPage): 회원가입 페이지 생성, 라우트 설정

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,3 +2,4 @@ export { default as TemplateEditPage } from './TemplateEditPage/TemplateEditPage
 export { default as TemplateListPage } from './TemplateListPage/TemplateListPage';
 export { default as TemplatePage } from './TemplatePage/TemplatePage';
 export { default as TemplateUploadPage } from './TemplateUploadPage/TemplateUploadPage';
+export { default as SignupPage } from './SignupPage/SignupPage';
