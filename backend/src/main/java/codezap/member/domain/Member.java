d0666e48ab422fd3aeda980e77db460e79b6a10b
김경미 <109158497+kyum-q@users.noreply.github.com>
2024-07-31 15:35:23 +0900

    feat(member): Member 도메인 사용을 위한 domain 작성

---
+++ backend/src/main/java/codezap/member/domain/Member.java
@@ -0,0 +1,29 @@
+package codezap.member.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+}
