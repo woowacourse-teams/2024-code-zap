b3366026c8ecc655bf04512e13d140a20e375719
jayming66 <mjw8941@gmail.com>
2024-07-31 21:59:43 +0900

    feat(SignupPage): 회원가입 유효성 검증 hook 적용

--- frontend/src/pages/SignupPage/SignupPage.tsx
+++ frontend/src/pages/SignupPage/SignupPage.tsx
@@ -1,10 +1,24 @@
 import { passwordEyeIcon } from '@/assets/images';
 import { Button, Flex, Input } from '@/components';
-import { useShowPassword } from '@/hooks/authentication';
+import { useShowPassword, useSignupForm } from '@/hooks/authentication';
 
 const SignupPage = () => {
   const { showPassword, handlePasswordToggle } = useShowPassword();
   const { showPassword: showPasswordConfirm, handlePasswordToggle: handlePasswordConfirmToggle } = useShowPassword();
+
+  const {
+    email,
+    nickname,
+    password,
+    confirmPassword,
+    errors,
+    handleEmailChange,
+    handleNicknameChange,
+    handlePasswordChange,
+    handleConfirmPasswordChange,
+    isFormValid,
+    handleSubmit,
+  } = useSignupForm();
 
   return (
     <>
@@ -14,28 +28,43 @@ const SignupPage = () => {
 
           <Input variant='outlined' size='medium'>
             <Input.Label>이메일</Input.Label>
-            <Input.TextField type='email' />
+            <Input.TextField type='email' value={email} onChange={handleEmailChange} />
+            {errors.email && <Input.HelperText>{errors.email}</Input.HelperText>}
           </Input>
+
           <Input variant='outlined' size='medium'>
             <Input.Label>닉네임</Input.Label>
-            <Input.TextField type='text' />
+            <Input.TextField type='text' value={nickname} onChange={handleNicknameChange} />
+            {errors.nickname && <Input.HelperText>{errors.nickname}</Input.HelperText>}
           </Input>
+
           <Input variant='outlined' size='medium'>
             <Input.Label>비밀번호</Input.Label>
-            <Input.TextField type={showPassword ? 'text' : 'password'} />
+            <Input.TextField
+              type={showPassword ? 'text' : 'password'}
+              value={password}
+              onChange={handlePasswordChange}
+            />
             <Input.Adornment>
               <img src={passwordEyeIcon} onClick={handlePasswordToggle} style={{ cursor: 'pointer' }} />
             </Input.Adornment>
+            {errors.password && <Input.HelperText>{errors.password}</Input.HelperText>}
           </Input>
+
           <Input variant='outlined' size='medium'>
             <Input.Label>비밀번호 확인</Input.Label>
-            <Input.TextField type={showPasswordConfirm ? 'text' : 'password'} />
+            <Input.TextField
+              type={showPasswordConfirm ? 'text' : 'password'}
+              value={confirmPassword}
+              onChange={handleConfirmPasswordChange}
+            />
             <Input.Adornment>
               <img src={passwordEyeIcon} onClick={handlePasswordConfirmToggle} style={{ cursor: 'pointer' }} />
             </Input.Adornment>
+            {errors.confirmPassword && <Input.HelperText>{errors.confirmPassword}</Input.HelperText>}
           </Input>
 
-          <Button variant='contained' size='filled' disabled>
+          <Button variant='contained' size='filled' disabled={!isFormValid()} onClick={handleSubmit}>
             회원가입
           </Button>
           <Flex justify='flex-end' width='100%' gap='1rem'>
