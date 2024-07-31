6b70cca069f82b419a69843f38fb154e69528c2c
jayming66 <mjw8941@gmail.com>
2024-07-31 21:59:11 +0900

    feat(authentication): useSignupForm 생성, http 요청 제외

---
+++ frontend/src/hooks/authentication/useSignupForm.ts
@@ -0,0 +1,92 @@
+import { useState } from 'react';
+
+export const useSignupForm = () => {
+  const [email, setEmail] = useState('');
+  const [nickname, setNickname] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [errors, setErrors] = useState({
+    email: '',
+    nickname: '',
+    password: '',
+    confirmPassword: '',
+  });
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return regex.test(email) ? '' : '유효한 이메일을 입력해주세요.';
+  };
+
+  const validateNickname = (nickname: string) => (nickname ? '' : '닉네임을 입력해주세요.');
+
+  const validatePassword = (password: string) => (password.length >= 6 ? '' : '비밀번호는 최소 6자 이상이어야 합니다.');
+
+  const validateConfirmPassword = (password: string, confirmPassword: string) =>
+    password === confirmPassword ? '' : '비밀번호가 일치하지 않습니다.';
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setEmail(value);
+    setErrors((prev) => ({ ...prev, email: validateEmail(value) }));
+  };
+
+  const handleNicknameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setNickname(value);
+    setErrors((prev) => ({ ...prev, nickname: validateNickname(value) }));
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setPassword(value);
+    setErrors((prev) => ({
+      ...prev,
+      password: validatePassword(value),
+      confirmPassword: validateConfirmPassword(value, confirmPassword),
+    }));
+  };
+
+  const handleConfirmPasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setConfirmPassword(value);
+    setErrors((prev) => ({
+      ...prev,
+      confirmPassword: validateConfirmPassword(password, value),
+    }));
+  };
+
+  const isFormValid = () =>
+    !errors.email &&
+    !errors.nickname &&
+    !errors.password &&
+    !errors.confirmPassword &&
+    email &&
+    nickname &&
+    password &&
+    confirmPassword;
+
+  const handleSubmit = async () => {
+    if (isFormValid()) {
+      console.log('valid');
+    }
+  };
+
+  return {
+    email,
+    nickname,
+    password,
+    confirmPassword,
+    errors,
+    handleEmailChange,
+    handleNicknameChange,
+    handlePasswordChange,
+    handleConfirmPasswordChange,
+    isFormValid,
+    handleSubmit,
+  };
+};
