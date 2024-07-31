5f2b2b3b6b5bb38b8595ba6356edb613abb3e553
jayming66 <mjw8941@gmail.com>
2024-07-31 21:27:27 +0900

    feat(Images): passwordEye 이미지 생성

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -4,3 +4,4 @@ export { default as pencilIcon } from './pencil.png';
 export { default as searchIcon } from './search.png';
 export { default as trashcanIcon } from './trashcan.png';
 export { default as userMenuIcon } from './userMenu_38x38.png';
+export { default as passwordEyeIcon } from './passwordEye.png';
