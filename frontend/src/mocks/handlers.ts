3607dc11900d9c65d5681f4b9e4bcf317957cd78
jayming66 <mjw8941@gmail.com>
2024-08-01 20:36:57 +0900

    refactor(mocks): templates 관련 MSW handler 코드 스타일 변경

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -6,16 +6,9 @@ import mockTemplate from './template.json';
 import mockTemplateList from './templateList.json';
 
 export const handlers = [
-  http.get(`${TEMPLATE_API_URL}`, () => {
-    const response = HttpResponse.json(mockTemplateList);
-
-    return response;
-  }),
-  http.get(`${TEMPLATE_API_URL}/:id`, () => {
-    const response = HttpResponse.json(mockTemplate);
-
-    return response;
-  }),
+  // templates
+  http.get(`${TEMPLATE_API_URL}`, () => HttpResponse.json(mockTemplateList)),
+  http.get(`${TEMPLATE_API_URL}/:id`, () => HttpResponse.json(mockTemplate)),
   http.post(`${TEMPLATE_API_URL}`, async () => HttpResponse.json({ status: 201 })),
   http.post(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 200 })),
   http.delete(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 204 })),
