5fdee4709ced53837e67d9782f7c8297983b9d7b
월하 <cerezo00@naver.com>
2024-08-01 12:51:41 +0900

    feat(CategoryButton): 카테고리 버튼 컴포넌트 구현

---
+++ frontend/src/components/CategoryButton/CategoryButton.style.ts
@@ -0,0 +1,13 @@
+import styled from '@emotion/styled';
+
+export const CategoryButtonWrapper = styled.button`
+  cursor: pointer;
+
+  width: 20rem;
+  height: 4.8rem;
+
+  background-color: white;
+  border: 1px solid #afb6c1;
+  border-radius: 8px;
+  box-shadow: 0 0.4rem 0.8rem #00000020;
+`;
