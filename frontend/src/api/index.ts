94f35159232178159a42efae1c48c64990196b95
월하 <cerezo00@naver.com>
2024-08-01 16:30:28 +0900

    feat(CategoryMenu): 카테고리 버튼 컴포넌트 확장

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,3 +1,4 @@
+export { CATEGORY } from './category';
 export { customFetch } from './customFetch';
 export { QUERY_KEY } from './queryKeys';
 export { TEMPLATE_API_URL, getTemplateList, getTemplate, postTemplate } from './templates';
