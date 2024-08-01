979d2c4d048a6a1127251f33ffcbb102724363b2
jayming66 <mjw8941@gmail.com>
2024-08-02 08:45:35 +0900

    feat(authentication): 로그인 기능 훅 생성, LoginPage에 적용

---
+++ frontend/src/hooks/authentication/useLoginForm.ts
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { postLogin } from '@/api/authentication';
+
+export const useLoginForm = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errors, setErrors] = useState({
+    email: '',
+    password: '',
+  });
+
+  const navigate = useNavigate();
+
+  const validateEmail = (email: string) => {
+    const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    return regex.test(email) ? '' : '유효한 이메일을 입력해주세요.';
+  };
+
+  const validatePassword = (password: string) => (password.length >= 6 ? '' : '비밀번호는 최소 6자 이상이어야 합니다.');
+
+  const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setEmail(value);
+    setErrors((prev) => ({ ...prev, email: validateEmail(value) }));
+  };
+
+  const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+
+    setPassword(value);
+    setErrors((prev) => ({ ...prev, password: validatePassword(value) }));
+  };
+
+  const isFormValid = () => !errors.email && !errors.password && email && password;
+
+  const handleSubmit = async () => {
+    if (isFormValid()) {
+      const response = await postLogin({ email, password });
+
+      if (!response.ok) {
+        console.error(response);
+      }
+
+      localStorage.setItem('token', response.headers.get('Authorization'));
+
+      navigate('/');
+    }
+  };
+
+  return {
+    email,
+    password,
+    errors,
+    handleEmailChange,
+    handlePasswordChange,
+    isFormValid,
+    handleSubmit,
+  };
+};
