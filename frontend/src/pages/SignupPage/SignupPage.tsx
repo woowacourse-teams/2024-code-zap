3ea8b0d3cc7d63072e2c2c3f1bf44e0a064fee5a
jayming66 <mjw8941@gmail.com>
2024-08-01 16:23:25 +0900

    refactor(SignupPage): HelperText의 높이 고정, 유효하지 않을 시 Input의 isValid 변경 처리

--- frontend/src/pages/SignupPage/SignupPage.tsx
+++ frontend/src/pages/SignupPage/SignupPage.tsx
@@ -25,22 +25,22 @@ const SignupPage = () => {
   return (
     <>
       <Flex direction='column' justify='center' align='center' height='100vh'>
-        <Flex direction='column' justify='center' align='center' gap='2.5rem' width='27.5rem'>
-          <h1 style={{ fontSize: '3rem', fontWeight: '700', color: '#F79037' }}>Code-Zap</h1>
+        <Flex direction='column' justify='center' align='center' gap='1rem' width='27.5rem'>
+          <h1 style={{ fontSize: '3rem', fontWeight: '400', color: '#F79037' }}>Hello Codezap</h1>
 
-          <Input variant='outlined' size='medium'>
+          <Input variant='outlined' size='medium' isValid={!errors.email}>
             <Input.Label>이메일</Input.Label>
             <Input.TextField type='email' value={email} onChange={handleEmailChange} />
-            {errors.email && <Input.HelperText>{errors.email}</Input.HelperText>}
+            <Input.HelperText>{errors.email}</Input.HelperText>
           </Input>
 
-          <Input variant='outlined' size='medium'>
+          <Input variant='outlined' size='medium' isValid={!errors.nickname}>
             <Input.Label>닉네임</Input.Label>
             <Input.TextField type='text' value={nickname} onChange={handleNicknameChange} />
-            {errors.nickname && <Input.HelperText>{errors.nickname}</Input.HelperText>}
+            <Input.HelperText>{errors.nickname}</Input.HelperText>
           </Input>
 
-          <Input variant='outlined' size='medium'>
+          <Input variant='outlined' size='medium' isValid={!errors.password}>
             <Input.Label>비밀번호</Input.Label>
             <Input.TextField
               type={showPassword ? 'text' : 'password'}
@@ -50,10 +50,10 @@ const SignupPage = () => {
             <Input.Adornment>
               <img src={passwordEyeIcon} onClick={handlePasswordToggle} style={{ cursor: 'pointer' }} />
             </Input.Adornment>
-            {errors.password && <Input.HelperText>{errors.password}</Input.HelperText>}
+            <Input.HelperText>{errors.password}</Input.HelperText>
           </Input>
 
-          <Input variant='outlined' size='medium'>
+          <Input variant='outlined' size='medium' isValid={!errors.confirmPassword}>
             <Input.Label>비밀번호 확인</Input.Label>
             <Input.TextField
               type={showPasswordConfirm ? 'text' : 'password'}
@@ -63,7 +63,7 @@ const SignupPage = () => {
             <Input.Adornment>
               <img src={passwordEyeIcon} onClick={handlePasswordConfirmToggle} style={{ cursor: 'pointer' }} />
             </Input.Adornment>
-            {errors.confirmPassword && <Input.HelperText>{errors.confirmPassword}</Input.HelperText>}
+            <Input.HelperText>{errors.confirmPassword}</Input.HelperText>
           </Input>
 
           <Button variant='contained' size='filled' disabled={!isFormValid()} onClick={handleSubmit}>
