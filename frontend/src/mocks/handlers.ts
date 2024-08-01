daf55094c993f4ab7d161826c776d3e253f5b25b
jayming66 <mjw8941@gmail.com>
2024-08-02 08:44:18 +0900

    feat(mocks): 로그인 요청 MSW handler 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,6 +1,6 @@
 import { HttpResponse, http } from 'msw';
 
-import { CHECK_EMAIL_API_URL, CHECK_USERNAME_API_URL, SIGNUP_API_URL } from '@/api/authentication';
+import { CHECK_EMAIL_API_URL, CHECK_USERNAME_API_URL, LOGIN_API_URL, SIGNUP_API_URL } from '@/api/authentication';
 import { TEMPLATE_API_URL } from '@/api/templates';
 import mockTemplate from './template.json';
 import mockTemplateList from './templateList.json';
@@ -15,6 +15,17 @@ export const handlers = [
 
   // authentication
   http.post(`${SIGNUP_API_URL}`, async () => HttpResponse.json({ status: 201 })),
+  http.post(`${LOGIN_API_URL}`, () => {
+    const mockToken =
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c';
+
+    return new HttpResponse(null, {
+      status: 200,
+      headers: {
+        Authorization: `Bearer ${mockToken}`,
+      },
+    });
+  }),
   http.get(`${CHECK_EMAIL_API_URL}`, async () => HttpResponse.json({ check: true })),
   http.get(`${CHECK_USERNAME_API_URL}`, async () => HttpResponse.json({ check: true })),
 ];
