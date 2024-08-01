d713868e1bc9a19680502b1fd2c496daf6992c31
zangsu <zangsu_@naver.com>
2024-08-01 13:10:02 +0900

    docs(category): 카테고리 기능 swagger 문서화

--- backend/src/main/java/codezap/category/dto/request/UpdateCategoryRequest.java
+++ backend/src/main/java/codezap/category/dto/request/UpdateCategoryRequest.java
@@ -5,7 +5,10 @@ import jakarta.validation.constraints.Size;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-public record CreateCategoryRequest(
+public record UpdateCategoryRequest(
+        @Schema(description = "카테고리 식별자", example = "1")
+        @NotNull(message = "카테고리 id가 null 입니다.")
+        Long id,
         @Schema(description = "카테고리 이름", example = "Spring")
         @NotNull(message = "카테고리 이름이 null 입니다.")
         @Size(max = 255, message = "카테고리 이름은 최대 255자까지 입력 가능합니다.")
