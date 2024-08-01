e24baa113924f11e47810f40a8a381f74f032794
jayming66 <mjw8941@gmail.com>
2024-08-01 20:27:05 +0900

    feat(api): 중복 이메일, 중복 닉네임 확인 API 요청 함수 생성

--- frontend/src/api/authentication.ts
+++ frontend/src/api/authentication.ts
@@ -3,7 +3,9 @@ import { customFetch } from './customFetch';
 
 const API_URL = process.env.REACT_APP_API_URL;
 
-export const SIGNUP_API_URL = `${API_URL}/signup`;
+const SIGNUP_API_URL = `${API_URL}/signup`;
+const CHECK_USERNAME_API_URL = `${API_URL}/check-username`;
+const CHECK_EMAIL_API_URL = `${API_URL}/check-email`;
 
 export const postSignup = async (signupInfo: SignupRequest) =>
   await customFetch({
@@ -11,3 +13,21 @@ export const postSignup = async (signupInfo: SignupRequest) =>
     url: `${SIGNUP_API_URL}`,
     body: JSON.stringify(signupInfo),
   });
+
+export const checkUsername = async (username: string) => {
+  const params = new URLSearchParams({ username });
+  const url = `${CHECK_USERNAME_API_URL}?${params}`;
+
+  return await customFetch({
+    url,
+  });
+};
+
+export const checkEmail = async (email: string) => {
+  const params = new URLSearchParams({ email });
+  const url = `${CHECK_EMAIL_API_URL}?${params}`;
+
+  return await customFetch({
+    url,
+  });
+};
