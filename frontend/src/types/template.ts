b586db0cc3786650779e2cc674ffe4039fdaee9f
월하 <cerezo00@naver.com>
2024-08-01 12:35:00 +0900

    refactor(types): Template, 타입 변경 및 Category, Tag 타입 정의

--- frontend/src/types/template.ts
+++ frontend/src/types/template.ts
@@ -5,10 +5,23 @@ export interface Snippet {
   ordinal: number;
 }
 
+export interface Tag {
+  id: number;
+  name: string;
+}
+
+export interface Category {
+  id: number;
+  name: string;
+}
+
 export interface Template {
   id: number;
   title: string;
+  description: string;
   snippets: Snippet[];
+  category: Category;
+  tags: Tag[];
   modifiedAt: string;
 }
 
