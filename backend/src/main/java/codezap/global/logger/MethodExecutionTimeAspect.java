c34ac983ce2e43ce0cbf8bdcd3ecd6d96b46d8e8
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-02 14:33:56 +0900

    feat(logger): 메서드 이름 및 실행 시간 측정을 위한 Aspect 생성

---
+++ backend/src/main/java/codezap/global/logger/MethodExecutionTimeAspect.java
@@ -0,0 +1,39 @@
+package codezap.global.logger;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Aspect
+@Component
+public class MethodExecutionTimeAspect {
+
+    @Around("execution(* codezap..*(..)) && " +
+            "!execution(* codezap.global.logger.MethodExecutionTimeAspect(..))" +
+            "!execution(* codezap.global.exception.*.*(..))")
+    public Object logExecutionTime(ProceedingJoinPoint joinPoint) throws Throwable {
+        if (!log.isInfoEnabled()) {
+            return joinPoint.proceed();
+        }
+
+        long startTime = System.currentTimeMillis();
+        Object result = joinPoint.proceed();
+        long endTime = System.currentTimeMillis();
+
+        long executionTimeMillis = endTime - startTime;
+
+        String className = joinPoint.getSignature()
+                .getDeclaringType()
+                .getSimpleName();
+        String methodName = joinPoint.getSignature()
+                .getName();
+
+        log.info("{}.{} 실행 {}ms", className, methodName, executionTimeMillis);
+
+        return result;
+    }
+}
