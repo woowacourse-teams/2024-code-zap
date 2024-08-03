00b71e926e86461830a75090c310fa599cba9db2
jayming66 <mjw8941@gmail.com>
2024-08-03 16:05:50 +0900

    refactor(SignupPage): form 태그 추가, gap 변경
    form 태그를 추가하지 않으면 개발자도구에서 password input을 form태그 안에 두라고 warning이 뜸

--- frontend/src/pages/SignupPage/SignupPage.tsx
+++ frontend/src/pages/SignupPage/SignupPage.tsx
@@ -27,63 +27,74 @@ const SignupPage = () => {
   return (
     <>
       <Flex direction='column' justify='center' align='center' height='100vh'>
-        <Flex direction='column' justify='center' align='center' gap='1rem' width='27.5rem'>
+        <Flex direction='column' justify='center' align='center' width='27.5rem' gap='3rem'>
           <h1 style={{ fontSize: '3rem', fontWeight: '400', color: '#F79037' }}>Hello Codezap</h1>
 
-          <Input variant='outlined' size='medium' isValid={!errors.email}>
-            <Input.Label>이메일</Input.Label>
-            <Input.TextField type='email' value={email} onChange={handleEmailChange} onBlur={() => checkEmailQuery()} />
-            <Input.HelperText>{errors.email}</Input.HelperText>
-          </Input>
+          <form
+            onSubmit={handleSubmit}
+            style={{ display: 'flex', flexDirection: 'column', width: '100%', height: '100%', gap: '1rem' }}
+          >
+            <Input variant='outlined' size='medium' isValid={!errors.email}>
+              <Input.Label>이메일</Input.Label>
+              <Input.TextField
+                type='email'
+                value={email}
+                onChange={handleEmailChange}
+                onBlur={() => checkEmailQuery()}
+              />
+              <Input.HelperText>{errors.email}</Input.HelperText>
+            </Input>
 
-          <Input variant='outlined' size='medium' isValid={!errors.username}>
-            <Input.Label>닉네임</Input.Label>
-            <Input.TextField
-              type='text'
-              value={username}
-              onChange={handleUsernameChange}
-              onBlur={() => checkUserQuery()}
-            />
-            <Input.HelperText>{errors.username}</Input.HelperText>
-          </Input>
+            <Input variant='outlined' size='medium' isValid={!errors.username}>
+              <Input.Label>닉네임</Input.Label>
+              <Input.TextField
+                type='text'
+                value={username}
+                onChange={handleUsernameChange}
+                onBlur={() => checkUserQuery()}
+              />
+              <Input.HelperText>{errors.username}</Input.HelperText>
+            </Input>
 
-          <Input variant='outlined' size='medium' isValid={!errors.password}>
-            <Input.Label>비밀번호</Input.Label>
-            <Input.TextField
-              type={showPassword ? 'text' : 'password'}
-              value={password}
-              onChange={handlePasswordChange}
-            />
-            <Input.Adornment>
-              <img src={passwordEyeIcon} onClick={handlePasswordToggle} style={{ cursor: 'pointer' }} />
-            </Input.Adornment>
-            <Input.HelperText>{errors.password}</Input.HelperText>
-          </Input>
+            <Input variant='outlined' size='medium' isValid={!errors.password}>
+              <Input.Label>비밀번호</Input.Label>
+              <Input.TextField
+                type={showPassword ? 'text' : 'password'}
+                value={password}
+                onChange={handlePasswordChange}
+              />
+              <Input.Adornment>
+                <img src={passwordEyeIcon} onClick={handlePasswordToggle} style={{ cursor: 'pointer' }} />
+              </Input.Adornment>
+              <Input.HelperText>{errors.password}</Input.HelperText>
+            </Input>
 
-          <Input variant='outlined' size='medium' isValid={!errors.confirmPassword}>
-            <Input.Label>비밀번호 확인</Input.Label>
-            <Input.TextField
-              type={showPasswordConfirm ? 'text' : 'password'}
-              value={confirmPassword}
-              onChange={handleConfirmPasswordChange}
-            />
-            <Input.Adornment>
-              <img src={passwordEyeIcon} onClick={handlePasswordConfirmToggle} style={{ cursor: 'pointer' }} />
-            </Input.Adornment>
-            <Input.HelperText>{errors.confirmPassword}</Input.HelperText>
-          </Input>
+            <Input variant='outlined' size='medium' isValid={!errors.confirmPassword}>
+              <Input.Label>비밀번호 확인</Input.Label>
+              <Input.TextField
+                type={showPasswordConfirm ? 'text' : 'password'}
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+              />
+              <Input.Adornment>
+                <img src={passwordEyeIcon} onClick={handlePasswordConfirmToggle} style={{ cursor: 'pointer' }} />
+              </Input.Adornment>
+              <Input.HelperText>{errors.confirmPassword}</Input.HelperText>
+            </Input>
 
-          <Button variant='contained' size='filled' disabled={!isFormValid()} onClick={handleSubmit}>
-            회원가입
-          </Button>
-          <Flex justify='flex-end' width='100%' gap='1rem'>
-            <span style={{ fontSize: '0.875rem', fontWeight: '400' }}>이미 계정이 있으신가요?</span>
-            <Link to={'/login'}>
-              <Button variant='text' size='small'>
-                로그인
-              </Button>
-            </Link>
-          </Flex>
+            <Button variant='contained' size='filled' disabled={!isFormValid()} type='submit'>
+              회원가입
+            </Button>
+
+            <Flex justify='flex-end' width='100%' gap='1rem'>
+              <span style={{ fontSize: '0.875rem', fontWeight: '400' }}>이미 계정이 있으신가요?</span>
+              <Link to={'/login'}>
+                <Button variant='text' size='small'>
+                  로그인
+                </Button>
+              </Link>
+            </Flex>
+          </form>
         </Flex>
       </Flex>
     </>
