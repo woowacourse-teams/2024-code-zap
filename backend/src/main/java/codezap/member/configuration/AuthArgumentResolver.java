090bdce2ac3b54b229db8c259dbc21887a1864db
재우 <Akaps6768@gmail.com>
2024-08-04 11:26:40 +0900

    feat(configuration): 쿠키 인증 argument resolver 등록

---
+++ backend/src/main/java/codezap/member/configuration/AuthArgumentResolver.java
@@ -0,0 +1,34 @@
+package codezap.member.configuration;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+import codezap.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class AuthArgumentResolver implements HandlerMethodArgumentResolver {
+
+    private final MemberService memberService;
+
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        return parameter.hasParameterAnnotation(BasicAuthentication.class);
+    }
+
+    @Override
+    public Object resolveArgument(
+            MethodParameter parameter,
+            ModelAndViewContainer mavContainer,
+            NativeWebRequest webRequest,
+            WebDataBinderFactory binderFactory
+    ) {
+        HttpServletRequest request = (HttpServletRequest) webRequest.getNativeRequest();
+        return memberService.login(request.getCookies());
+    }
+}
---
+++ backend/src/main/java/codezap/member/configuration/BasicAuthentication.java
@@ -0,0 +1,4 @@
+package codezap.member.configuration;
+
+public @interface BasicAuthentication {
+}
