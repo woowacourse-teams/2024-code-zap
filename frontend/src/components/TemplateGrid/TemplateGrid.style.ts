61f68b550a575efcca0009d12f9d3596465e38a9
월하 <cerezo00@naver.com>
2024-08-04 14:08:50 +0900

    refactor(components): 카테고리메뉴, 템플릿그리드 컴포넌트 반응형 디자인

--- frontend/src/components/TemplateGrid/TemplateGrid.style.ts
+++ frontend/src/components/TemplateGrid/TemplateGrid.style.ts
@@ -4,5 +4,7 @@ export const TemplateContainer = styled.div<{ cols: number }>`
   display: grid;
   grid-gap: 1rem;
   grid-template-columns: repeat(${({ cols }) => cols}, 1fr);
-  width: 63.125rem;
+
+  width: 100%;
+  max-width: 63.125rem;
 `;
