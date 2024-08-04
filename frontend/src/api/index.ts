439bbdebeb3324274a5e02c00612355355aa224d
월하 <cerezo00@naver.com>
2024-08-04 15:23:47 +0900

    refactor(src): api 디렉터리의 index에서 export 및 변경사항 반영

--- frontend/src/api/index.ts
+++ frontend/src/api/index.ts
@@ -1,4 +1,11 @@
 export { CATEGORY } from './category';
 export { customFetch } from './customFetch';
 export { QUERY_KEY } from './queryKeys';
-export { TEMPLATE_API_URL, getTemplateList, getTemplate, postTemplate } from './templates';
+export {
+  TEMPLATE_API_URL,
+  getTemplateList,
+  getTemplate,
+  postTemplate,
+  editTemplate,
+  deleteTemplate,
+} from './templates';
