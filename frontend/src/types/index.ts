7fe61aea5a02a41bb1523c106207910924b6c299
월하 <cerezo00@naver.com>
2024-08-04 21:47:56 +0900

    feat(queries): 'useCategoryListQuery' 훅 구현

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -1,2 +1,2 @@
 export type { Snippet, Tag, Category, Template, TemplateListItem } from './template';
-export type { TemplateEditRequest, TemplateListResponse, TemplateUploadRequest } from './api';
+export type { TemplateEditRequest, TemplateListResponse, TemplateUploadRequest, CategoryListResponse } from './api';
