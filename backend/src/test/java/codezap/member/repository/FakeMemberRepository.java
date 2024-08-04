a2fc045616bb48393c3dbb4bfb8d1eba7104ade4
재우 <Akaps6768@gmail.com>
2024-08-04 15:02:26 +0900

    refactor(service): repository 구조 변경

--- backend/src/test/java/codezap/member/repository/FakeMemberRepository.java
+++ backend/src/test/java/codezap/member/repository/FakeMemberRepository.java
@@ -5,13 +5,6 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicLong;
-import java.util.function.Function;
-
-import org.springframework.data.domain.Example;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.repository.query.FluentQuery.FetchableFluentQuery;
 
 import codezap.member.domain.Member;
 
@@ -45,12 +38,7 @@ public class FakeMemberRepository implements MemberRepository {
     }
 
     @Override
-    public long count() {
-        return members.size();
-    }
-
-    @Override
-    public <S extends Member> S save(S entity) {
+    public Member save(Member entity) {
         var saved = new Member(
                 getOrGenerateId(entity),
                 entity.getEmail(),
@@ -59,7 +47,7 @@ public class FakeMemberRepository implements MemberRepository {
         );
         members.removeIf(member -> Objects.equals(member.getId(), entity.getId()));
         members.add(saved);
-        return (S) saved;
+        return saved;
     }
 
     private long getOrGenerateId(Member entity) {
@@ -69,142 +57,8 @@ public class FakeMemberRepository implements MemberRepository {
         return idCounter.getAndIncrement();
     }
 
-    @Override
-    public boolean existsById(Long aLong) {
-        return false;
-    }
-
-    @Override
-    public void flush() {
-    }
-
-    @Override
-    public <S extends Member> S saveAndFlush(S entity) {
-        return null;
-    }
-
-    @Override
-    public <S extends Member> List<S> saveAllAndFlush(Iterable<S> entities) {
-        return List.of();
-    }
-
-    @Override
-    public void deleteAllInBatch(Iterable<Member> entities) {
-
-    }
-
-    @Override
-    public void deleteAllByIdInBatch(Iterable<Long> longs) {
-
-    }
-
-    @Override
-    public void deleteAllInBatch() {
-
-    }
-
-    @Override
-    public Member getOne(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Member getById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public Member getReferenceById(Long aLong) {
-        return null;
-    }
-
-    @Override
-    public <S extends Member> Optional<S> findOne(Example<S> example) {
-        return Optional.empty();
-    }
-
-    @Override
-    public <S extends Member> List<S> findAll(Example<S> example) {
-        return List.of();
-    }
-
-    @Override
-    public <S extends Member> List<S> findAll(Example<S> example, Sort sort) {
-        return List.of();
-    }
-
-    @Override
-    public <S extends Member> Page<S> findAll(Example<S> example, Pageable pageable) {
-        return null;
-    }
-
-    @Override
-    public <S extends Member> long count(Example<S> example) {
-        return 0;
-    }
-
-    @Override
-    public <S extends Member> boolean exists(Example<S> example) {
-        return false;
-    }
-
-    @Override
-    public <S extends Member, R> R findBy(Example<S> example, Function<FetchableFluentQuery<S>, R> queryFunction) {
-        return null;
-    }
-
-    @Override
-    public <S extends Member> List<S> saveAll(Iterable<S> entities) {
-        return List.of();
-    }
-
-    @Override
-    public Optional<Member> findById(Long aLong) {
-        return Optional.empty();
-    }
-
-    @Override
-    public List<Member> findAll() {
-        return members;
-    }
-
-    @Override
-    public List<Member> findAllById(Iterable<Long> longs) {
-        return List.of();
-    }
-
-    @Override
-    public void deleteById(Long aLong) {
-
-    }
-
-    @Override
-    public void delete(Member entity) {
-
-    }
-
-    @Override
-    public void deleteAllById(Iterable<? extends Long> longs) {
-
-    }
-
-    @Override
-    public void deleteAll(Iterable<? extends Member> entities) {
-
-    }
-
-    @Override
-    public void deleteAll() {
-
-    }
-
-    @Override
-    public List<Member> findAll(Sort sort) {
-        return List.of();
-    }
-
-    @Override
-    public Page<Member> findAll(Pageable pageable) {
-        return null;
+    private boolean existsById(Long id) {
+        return members.stream()
+                .anyMatch(member -> Objects.equals(member.getId(), id));
     }
 }
