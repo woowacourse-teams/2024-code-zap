5f68396b2bc30e69086c93491c2ab109ab952aa6
월하 <cerezo00@naver.com>
2024-08-04 14:07:14 +0900

    refactor(components): 레이아웃, 헤더 좌우 여백 변경 (3rem > 2rem)

--- frontend/src/components/Layout/Layout.style.ts
+++ frontend/src/components/Layout/Layout.style.ts
@@ -3,10 +3,10 @@ import styled from '@emotion/styled';
 export const LayoutContainer = styled.div`
   --header-height: 4rem;
 
-  max-width: 86rem;
+  max-width: 84rem;
   margin: auto;
-  margin-bottom: 3rem;
-  padding: 0 3rem;
+  margin-bottom: 2rem;
+  padding: 0 2rem;
 `;
 
 export const Wrapper = styled.div`
