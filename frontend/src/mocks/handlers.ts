585fc48b587a722101c58d06d87ca8036c919a7b
월하 <cerezo00@naver.com>
2024-08-04 21:48:31 +0900

    test(mocks): 카테고리 리스트 요청 MSW 구현

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,6 +1,7 @@
 import { HttpResponse, http } from 'msw';
 
-import { TEMPLATE_API_URL } from '@/api';
+import { CATEGORY_API_URL, TEMPLATE_API_URL } from '@/api';
+import mockCategoryList from './categoryList.json';
 import mockTemplate from './template.json';
 import mockTemplateList from './templateList.json';
 
@@ -10,4 +11,5 @@ export const handlers = [
   http.post(`${TEMPLATE_API_URL}`, async () => HttpResponse.json({ status: 201 })),
   http.post(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 200 })),
   http.delete(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 204 })),
+  http.get(`${CATEGORY_API_URL}`, () => HttpResponse.json(mockCategoryList)),
 ];
