a4c063a4d543b3a98f01bbef2f9d2bffc97f2b24
jayming66 <mjw8941@gmail.com>
2024-08-05 20:55:12 +0900

    refactor(authentication): API 변경에 따른 response 반환값 제거

--- frontend/src/queries/authentication/useCheckEmailQuery.ts
+++ frontend/src/queries/authentication/useCheckEmailQuery.ts
@@ -2,11 +2,11 @@ import { UseQueryResult, useQuery } from '@tanstack/react-query';
 
 import { QUERY_KEY } from '@/api';
 import { checkEmail } from '@/api/authentication';
-import { CheckEmailResponse } from '@/types/authentication';
 
-export const useCheckEmailQuery = (email: string): UseQueryResult<CheckEmailResponse, Error> =>
-  useQuery<CheckEmailResponse, Error>({
+export const useCheckEmailQuery = (email: string): UseQueryResult =>
+  useQuery({
     queryKey: [QUERY_KEY.CHECK_EMAIL, email],
     queryFn: () => checkEmail(email),
     enabled: false,
+    retry: false,
   });
