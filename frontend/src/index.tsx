1c3d42b6859c6d639eb8a8c835087e0ff148d9b6
jayming66 <mjw8941@gmail.com>
2024-08-05 10:47:27 +0900

    feat(frontend): Sentry 초기 설정
    SENTRY_ORG, PROJECT, DSN,ENV는 공식문서에는 없지만 우선 secret으로 남김

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,4 +1,5 @@
 import { ThemeProvider } from '@emotion/react';
+import * as Sentry from '@sentry/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
@@ -9,6 +10,19 @@ import GlobalStyles from './style/GlobalStyles';
 import { theme } from './style/theme';
 
 const queryClient = new QueryClient();
+
+Sentry.init({
+  dsn: process.env.SENTRY_DSN,
+  environment: process.env.NODE_ENV,
+  integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
+  // Performance Monitoring
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ['localhost', /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
