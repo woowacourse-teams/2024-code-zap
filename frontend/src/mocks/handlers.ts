846c346b9238a87e971397c7913f8df34c361d6e
헤인 <hain7ce@gmail.com>
2024-08-05 18:12:35 +0900

    feat(mocks): categoryHandlers 추가

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -1,10 +1,12 @@
 import { HttpResponse, http } from 'msw';
 
+import { CATEGORY_API_URL } from '@/api';
 import { TEMPLATE_API_URL } from '@/api/templates';
+import mockCategoryList from './categoryList.json';
 import mockTemplate from './template.json';
 import mockTemplateList from './templateList.json';
 
-export const handlers = [
+const templateHandlers = [
   http.get(`${TEMPLATE_API_URL}`, () => {
     const response = HttpResponse.json(mockTemplateList);
 
@@ -19,3 +21,14 @@ export const handlers = [
   http.post(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 200 })),
   http.delete(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 204 })),
 ];
+
+const categoryHandlers = [
+  http.get(`${CATEGORY_API_URL}`, () => {
+    const response = HttpResponse.json(mockCategoryList);
+
+    return response;
+  }),
+  http.post(`${CATEGORY_API_URL}`, async () => HttpResponse.json({ status: 201 })),
+];
+
+export const handlers = [...templateHandlers, ...categoryHandlers];
