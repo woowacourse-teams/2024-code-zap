cdee61997d6adeca5256c49d522015668fd21fce
월하 <cerezo00@naver.com>
2024-08-05 16:38:44 +0900

    test(mocks): 카테고리 요청에 대한 MSW 핸들러 작성

--- frontend/src/mocks/handlers.ts
+++ frontend/src/mocks/handlers.ts
@@ -6,7 +6,25 @@ import mockTemplate from './template.json';
 import mockTemplateList from './templateList.json';
 
 export const handlers = [
-  http.get(`${TEMPLATE_API_URL}`, () => HttpResponse.json(mockTemplateList)),
+  http.get(`${TEMPLATE_API_URL}`, (req) => {
+    const url = new URL(req.request.url);
+    const categoryId = url.searchParams.get('category');
+    const tagId = url.searchParams.get('tag');
+
+    let filteredTemplates = mockTemplateList.templates;
+
+    if (categoryId) {
+      filteredTemplates = filteredTemplates.filter((template) => template.category.id.toString() === categoryId);
+    }
+
+    if (tagId) {
+      filteredTemplates = filteredTemplates.filter((template) =>
+        template.tags.some((tag) => tag.id.toString() === tagId),
+      );
+    }
+
+    return HttpResponse.json({ templates: filteredTemplates });
+  }),
   http.get(`${TEMPLATE_API_URL}/:id`, () => HttpResponse.json(mockTemplate)),
   http.post(`${TEMPLATE_API_URL}`, async () => HttpResponse.json({ status: 201 })),
   http.post(`${TEMPLATE_API_URL}/:id`, async () => HttpResponse.json({ status: 200 })),
