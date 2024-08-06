a97311e1d2e98b4208313c6ce4c49ea78eba6119
재우 <Akaps6768@gmail.com>
2024-08-06 17:09:25 +0900

    fix(configuration): cors 설정 추가

--- backend/src/main/java/codezap/global/cors/WebCorsConfiguration.java
+++ backend/src/main/java/codezap/global/cors/WebCorsConfiguration.java
@@ -9,6 +9,7 @@ public class WebCorsConfiguration implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
+                .allowCredentials(true)
                 .allowedOrigins("*")
                 .allowedMethods("*");
     }
