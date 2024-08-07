387996c3d34462ebeceb409155bdf7cd28d1acb8
jayming66 <mjw8941@gmail.com>
2024-08-07 22:24:31 +0900

    feat(category): 카테고리 목록 조회 요청에 memberId Params 추가

--- frontend/src/queries/category/useCategoryListQuery.ts
+++ frontend/src/queries/category/useCategoryListQuery.ts
@@ -1,10 +1,16 @@
 import { useQuery } from '@tanstack/react-query';
 
 import { QUERY_KEY, getCategoryList } from '@/api';
+import { useAuth } from '@/hooks/authentication/useAuth';
 import type { CategoryListResponse } from '@/types';
 
-export const useCategoryListQuery = () =>
-  useQuery<CategoryListResponse, Error>({
+export const useCategoryListQuery = () => {
+  const {
+    memberInfo: { memberId },
+  } = useAuth();
+
+  return useQuery<CategoryListResponse, Error>({
     queryKey: [QUERY_KEY.CATEGORY_LIST],
-    queryFn: getCategoryList,
+    queryFn: () => getCategoryList({ memberId }),
   });
+};
