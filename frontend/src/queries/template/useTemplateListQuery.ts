d8e44fc46452790addd0c4cb8e78c66c9521647f
월하 <cerezo00@naver.com>
2024-08-07 18:52:41 +0900

    feat(src): '내 템플릿' 페이지 정렬 기능 구현

--- frontend/src/queries/template/useTemplateListQuery.ts
+++ frontend/src/queries/template/useTemplateListQuery.ts
@@ -1,19 +1,27 @@
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 
-import { PAGE_SIZE, QUERY_KEY, getTemplateList } from '@/api';
-import type { TemplateListResponse } from '@/types';
+import { PAGE_SIZE, QUERY_KEY, getTemplateList, DEFAULT_SORTING_OPTION } from '@/api';
+import type { TemplateListResponse, SortingKey } from '@/types';
 
 interface Props {
+  keyword?: string;
   categoryId?: number;
   tagIds?: number[];
+  sort?: SortingKey;
   page?: number;
   pageSize?: number;
-  keyword?: string;
 }
 
-export const useTemplateListQuery = ({ categoryId, tagIds, page = 1, pageSize = PAGE_SIZE, keyword }: Props) =>
+export const useTemplateListQuery = ({
+  keyword,
+  categoryId,
+  tagIds,
+  sort = DEFAULT_SORTING_OPTION.key,
+  page = 1,
+  pageSize = PAGE_SIZE,
+}: Props) =>
   useQuery<TemplateListResponse, Error>({
-    queryKey: [QUERY_KEY.TEMPLATE_LIST, categoryId, tagIds, page, pageSize, keyword],
-    queryFn: () => getTemplateList({ categoryId, tagIds, page, pageSize, keyword }),
+    queryKey: [QUERY_KEY.TEMPLATE_LIST, keyword, categoryId, tagIds, sort, page, pageSize],
+    queryFn: () => getTemplateList({ keyword, categoryId, tagIds, sort, page, pageSize }),
     placeholderData: keepPreviousData,
   });
