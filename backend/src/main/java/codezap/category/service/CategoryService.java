f8c9eda54c01926cd62b62fd5f69f4788eaa99c8
HoeSeong123 <ghltjd3@gmail.com>
2024-08-08 12:04:55 +0900

    fix(service): 카테고리 전체 조회시 id 오름차순으로 정렬하여 응답하도록 변경

--- backend/src/main/java/codezap/category/service/CategoryService.java
+++ backend/src/main/java/codezap/category/service/CategoryService.java
@@ -1,5 +1,9 @@
 package codezap.category.service;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -42,7 +46,9 @@ public class CategoryService {
 
     public FindAllCategoriesResponse findAllByMember(Long memberId) {
         Member member = memberJpaRepository.fetchById(memberId);
-        return FindAllCategoriesResponse.from(categoryRepository.findAllByMember(member));
+        List<Category> categories = new ArrayList<>(categoryRepository.findAllByMember(member));
+        categories.sort(Comparator.comparing(Category::getId));
+        return FindAllCategoriesResponse.from(categories);
     }
 
     public FindAllCategoriesResponse findAll() {
