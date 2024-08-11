b0fa29e3ef974f1b88507248d3618a3a3cb9caa8
HoeSeong123 <ghltjd3@gmail.com>
2024-08-11 22:41:33 +0900

    docs: 스웨거에서 memberDto 안보이도록 설정

--- backend/src/main/java/codezap/global/swagger/SpringDocConfiguration.java
+++ backend/src/main/java/codezap/global/swagger/SpringDocConfiguration.java
@@ -1,5 +1,8 @@
 package codezap.global.swagger;
 
+import java.util.List;
+
+import org.springdoc.core.customizers.OperationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -21,7 +24,21 @@ public class SpringDocConfiguration {
                         """);
 
         return new OpenAPI()
-                .addServersItem(new Server().url("https://api.code-zap.com"))
+                .servers(
+                        List.of(
+                                new Server()
+                                        .url("https://api.code-zap.com")
+                                        .description("클라우드에 배포되어 있는 테스트 서버입니다."),
+                                new Server()
+                                        .url("http://localhost:8080")
+                                        .description("BE 팀에서 사용할 로컬 환경 테스트를 위한 서버입니다.")
+                        )
+                )
                 .info(info);
+    }
+
+    @Bean
+    public OperationCustomizer addCommonErrorResponses() {
+        return new AuthOperationCustomizer();
     }
 }
