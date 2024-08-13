0d4e4c2aa887f03d55b05ce5320ca6b091b1f44e
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-14 00:46:38 +0900

    docs(response): schema 작성

--- backend/src/main/java/codezap/member/dto/response/UpdateMemberProfileResponse.java
+++ backend/src/main/java/codezap/member/dto/response/UpdateMemberProfileResponse.java
@@ -1,7 +1,12 @@
 package codezap.member.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 public record UpdateMemberProfileResponse(
+        @Schema(description = "이메일", example = "code@zap.com")
         String email,
+
+        @Schema(description = "사용자명", example = "zappy")
         String username
 ) {
 }
