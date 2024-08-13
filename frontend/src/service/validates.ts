80e0aeb43c6ac172f403ea45e697e500b8720312
jayming66 <mjw8941@gmail.com>
2024-08-13 15:02:22 +0900

    feat(service); 비밀번호 유효성 검사 수정

--- frontend/src/service/validates.ts
+++ frontend/src/service/validates.ts
@@ -7,11 +7,19 @@ export const validateEmail = (email: string) => {
 export const validateUsername = (username: string) => {
   const regex = /^[a-zA-Z0-9가-힣-_]+$/;
 
-  return regex.test(username) && username.length > 0 ? '' : '공백을 제외한 1자 이상의 닉네임을 입력해주세요.';
+  return regex.test(username) && username.length > 0 ? '' : '1자 이상의 닉네임을 입력해주세요.';
 };
 
-export const validatePassword = (password: string) =>
-  password.length >= 6 && !/\s/.test(password) ? '' : '공백을 제외한 6자 이상의 비밀번호를 입력해주세요.';
+export const validatePassword = (password: string) => {
+  const hasLetters = /[a-zA-Z]/.test(password);
+  const hasNumbers = /[0-9]/.test(password);
+  const hasNoSpaces = !/\s/.test(password);
+  const isValidLength = password.length >= 8;
+
+  return hasLetters && hasNumbers && isValidLength && hasNoSpaces
+    ? ''
+    : '영문자, 숫자를 포함한 8자 이상의 비밀번호를 입력해주세요.';
+};
 
 export const validateConfirmPassword = (password: string, confirmPassword: string) =>
   password === confirmPassword ? '' : '비밀번호가 일치하지 않습니다.';
