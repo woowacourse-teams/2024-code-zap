647f1c592efe2c5a8b9bcb5f750c47c0868a64ec
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-13 10:45:45 +0900

    refactor(exception): methodArgumentNotValid override

--- backend/src/main/java/codezap/global/exception/GlobalExceptionHandler.java
+++ backend/src/main/java/codezap/global/exception/GlobalExceptionHandler.java
@@ -3,13 +3,16 @@ package codezap.global.exception;
 import java.util.List;
 
 import org.springframework.context.support.DefaultMessageSourceResolvable;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import lombok.extern.slf4j.Slf4j;
@@ -28,9 +31,9 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
                 );
     }
 
-    @ExceptionHandler
-    public ResponseEntity<ProblemDetail> handleMethodArgumentNotValidException(
-            MethodArgumentNotValidException exception
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatusCode status, WebRequest request
     ) {
         BindingResult bindingResult = exception.getBindingResult();
         List<String> errorMessages = bindingResult.getAllErrors().stream()
