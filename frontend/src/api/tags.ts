9015f971fd026b5241e333912808779ef38338d5
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-08-14 14:26:55 +0900

    refactor(api): url 에서 불필요한 슬래시 제거

--- frontend/src/api/tags.ts
+++ frontend/src/api/tags.ts
@@ -7,7 +7,7 @@ const API_URL = process.env.REACT_APP_API_URL;
 export const TAG_API_URL = `${API_URL}/templates/tags`;
 
 export const getTagList = async ({ memberId }: Pick<MemberInfo, 'memberId'>) => {
-  const url = `${TAG_API_URL}/?memberId=${memberId}`;
+  const url = `${TAG_API_URL}?memberId=${memberId}`;
 
   const response = await customFetch<TagListResponse>({
     url,
