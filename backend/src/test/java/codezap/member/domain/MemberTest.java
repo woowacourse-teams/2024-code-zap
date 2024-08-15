b90c4b31d3a98246dab5f9d316e3da2220fabcad
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-16 02:06:36 +0900

    refactor(domain): 회원 비밀번호 테스트 추가

---
+++ backend/src/test/java/codezap/member/domain/MemberTest.java
@@ -0,0 +1,25 @@
+package codezap.member.domain;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import codezap.member.fixture.MemberFixture;
+
+class MemberTest {
+
+    @Test
+    @DisplayName("비밀번호 일치")
+    void matchPassword() {
+        Member member = MemberFixture.memberFixture();
+        assertTrue(member.matchPassword(member.getPassword()));
+    }
+
+    @Test
+    @DisplayName("비밀번호 불일치")
+    void matchPassword_Fail_NotSamePassword() {
+        Member member = MemberFixture.memberFixture();
+        assertFalse(member.matchPassword(member.getPassword() + "wrong"));
+    }
+}
