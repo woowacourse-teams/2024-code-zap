180ae46e5b595d5c28681c5e927c42e65efd28bf
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-15 23:17:20 +0900

    feat(repository): findByEmail -> fetchByEmail

--- backend/src/main/java/codezap/member/repository/MemberRepository.java
+++ backend/src/main/java/codezap/member/repository/MemberRepository.java
@@ -2,6 +2,9 @@ package codezap.member.repository;
 
 import java.util.Optional;
 
+import org.springframework.http.HttpStatus;
+
+import codezap.global.exception.CodeZapException;
 import codezap.member.domain.Member;
 
 public interface MemberRepository {
@@ -15,6 +18,11 @@ public interface MemberRepository {
     boolean existsById(Long id);
 
     Optional<Member> findByEmail(String email);
+
+    default Member fetchByEmail(String email) {
+        return findByEmail(email)
+                .orElseThrow(() -> new CodeZapException(HttpStatus.UNAUTHORIZED, "존재하지 않는 이메일 " + email + " 입니다."));
+    }
 
     Member save(Member member);
 }
