913410fa143e81395de447ef59d909b99eebacd6
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-15 21:34:19 +0900

    refactor(provider): 인증 값을 제공하는 객체 추상화 및 Basic Auth 구현체 생성

---
+++ backend/src/main/java/codezap/auth/provider/BasicAuthCredentialProvider.java
@@ -0,0 +1,41 @@
+package codezap.auth.provider;
+
+import java.nio.charset.StandardCharsets;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+import codezap.auth.BasicAuthDecoder;
+import codezap.global.exception.CodeZapException;
+import codezap.member.domain.Member;
+import codezap.member.repository.MemberRepository;
+
+@Component
+public class BasicAuthCredentialProvider implements CredentialProvider {
+
+    private final MemberRepository memberRepository;
+
+    public BasicAuthCredentialProvider(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public String createCredential(Member member) {
+        return HttpHeaders.encodeBasicAuth(member.getEmail(), member.getPassword(), StandardCharsets.UTF_8);
+    }
+
+    @Override
+    public Member extractMember(String credential) {
+        String[] emailAndPassword = BasicAuthDecoder.decodeBasicAuth(credential);
+        Member member = memberRepository.fetchByEmail(emailAndPassword[0]);
+        checkMatchPassword(member, emailAndPassword[1]);
+        return member;
+    }
+
+    private void checkMatchPassword(Member member, String password) {
+        if (!member.matchPassword(password)) {
+            throw new CodeZapException(HttpStatus.UNAUTHORIZED, "이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
+    }
+}
---
+++ backend/src/test/java/codezap/auth/provider/BasicAuthCredentialProviderTest.java
@@ -0,0 +1,79 @@
+package codezap.auth.provider;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.nio.charset.StandardCharsets;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.http.HttpHeaders;
+
+import codezap.global.exception.CodeZapException;
+import codezap.member.domain.Member;
+import codezap.member.fixture.MemberFixture;
+import codezap.member.repository.FakeMemberRepository;
+import codezap.member.repository.MemberRepository;
+
+class BasicAuthCredentialProviderTest {
+
+    private final MemberRepository memberRepository = new FakeMemberRepository();
+
+    private BasicAuthCredentialProvider basicAuthCredentialProvider;
+
+    @BeforeEach
+    void setUp() {
+        basicAuthCredentialProvider = new BasicAuthCredentialProvider(memberRepository);
+    }
+
+    @Test
+    @DisplayName("BasicAuth 인증 정보 생성 성공")
+    void createCredential() {
+        Member member = MemberFixture.memberFixture();
+        String actualCredential = basicAuthCredentialProvider.createCredential(member);
+
+        String expectedCredential = HttpHeaders.encodeBasicAuth(member.getEmail(), member.getPassword(),
+                StandardCharsets.UTF_8);
+        assertEquals(actualCredential, expectedCredential);
+    }
+
+    @Nested
+    class 회원_추출_성공 {
+        @Test
+        @DisplayName("회원 추출 성공")
+        void extractMember() {
+            Member member = memberRepository.save(MemberFixture.memberFixture());
+            String credential = basicAuthCredentialProvider.createCredential(member);
+
+            assertEquals(member, basicAuthCredentialProvider.extractMember(credential));
+        }
+
+        @Test
+        @DisplayName("회원 추출 실패: 존재하지 않는 회원")
+        void extractMemberThrowNotExistMember() {
+            Member unsaverdMember = MemberFixture.memberFixture();
+            String credential = basicAuthCredentialProvider.createCredential(unsaverdMember);
+
+            assertThatThrownBy(() -> basicAuthCredentialProvider.extractMember(credential))
+                    .isInstanceOf(CodeZapException.class)
+                    .hasMessage("존재하지 않는 이메일 " + unsaverdMember.getEmail() + " 입니다.");
+        }
+
+        @ParameterizedTest
+        @ValueSource(strings = {"wrongPassword", " "})
+        @DisplayName("회원 추출 실패: 잘못된 비밀번호로 생성된 인증 값")
+        void extractMemberThrow(String wrongPassword) {
+            Member savedMember = memberRepository.save(MemberFixture.memberFixture());
+            String wrongCredential = HttpHeaders.encodeBasicAuth(savedMember.getEmail(), wrongPassword,
+                    StandardCharsets.UTF_8);
+
+            assertThatThrownBy(() -> basicAuthCredentialProvider.extractMember(wrongCredential))
+                    .isInstanceOf(CodeZapException.class)
+                    .hasMessage("이메일 또는 비밀번호가 일치하지 않습니다.");
+        }
+    }
+}
