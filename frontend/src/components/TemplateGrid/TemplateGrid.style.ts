016d5cc83d674a5089a8fdf8075460f285134c70
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-08-16 14:01:24 +0900

    feat(src): 템플릿 선택 삭제 기능 추가

--- frontend/src/components/TemplateGrid/TemplateGrid.style.ts
+++ frontend/src/components/TemplateGrid/TemplateGrid.style.ts
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 
+import { theme } from '@/style/theme';
+
 export const TemplateContainer = styled.div<{ cols: number }>`
   display: grid;
   grid-gap: 1rem;
@@ -7,4 +9,30 @@ export const TemplateContainer = styled.div<{ cols: number }>`
 
   width: 100%;
   max-width: 63.125rem;
+`;
+
+export const TemplateCardWrapper = styled.div<{ isSelected: boolean }>`
+  position: relative;
+  width: 100%;
+  border-radius: 8px;
+
+  &::before {
+    pointer-events: none;
+    content: '';
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    border-radius: 8px;
+    box-shadow: ${(props) => (props.isSelected ? `inset 0 0 0 3px ${theme.color.light.primary_500}` : 'none')};
+
+    transition: box-shadow 0.2s ease-in-out;
+  }
+`;
+
+export const NonInteractiveWrapper = styled.div`
+  pointer-events: none;
 `;
