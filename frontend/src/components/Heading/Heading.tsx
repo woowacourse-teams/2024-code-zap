f0917f4148540034f5b2dca6125c29b7cba5572e
월하 <cerezo00@naver.com>
2024-08-16 18:24:34 +0900

    feat(components): Heading, Text 컴포넌트 textDecoration 속성 추가

--- frontend/src/components/Heading/Heading.tsx
+++ frontend/src/components/Heading/Heading.tsx
@@ -8,38 +8,41 @@ type As = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p' | 'span';
 const weights = { ...theme.font.weight };
 const sizes = { ...theme.font.size.heading };
 
+type TextDecoration = 'none' | 'underline' | 'overline' | 'line-through';
+
 interface Props {
   as?: As;
   color: string;
   weight?: keyof typeof weights;
+  textDecoration?: TextDecoration;
 }
 
-const XLarge = ({ children, as = 'span', color, weight = 'bold' }: PropsWithChildren<Props>) => (
-  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.xlarge}>
+const XLarge = ({ children, as = 'span', color, weight = 'bold', textDecoration }: PropsWithChildren<Props>) => (
+  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.xlarge} textDecoration={textDecoration}>
     {children}
   </S.HeadingElement>
 );
 
-const Large = ({ children, as = 'span', color, weight = 'bold' }: PropsWithChildren<Props>) => (
-  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.large}>
+const Large = ({ children, as = 'span', color, weight = 'bold', textDecoration }: PropsWithChildren<Props>) => (
+  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.large} textDecoration={textDecoration}>
     {children}
   </S.HeadingElement>
 );
 
-const Medium = ({ children, as = 'span', color, weight = 'bold' }: PropsWithChildren<Props>) => (
-  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.medium}>
+const Medium = ({ children, as = 'span', color, weight = 'bold', textDecoration }: PropsWithChildren<Props>) => (
+  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.medium} textDecoration={textDecoration}>
     {children}
   </S.HeadingElement>
 );
 
-const Small = ({ children, as = 'span', color, weight = 'bold' }: PropsWithChildren<Props>) => (
-  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.small}>
+const Small = ({ children, as = 'span', color, weight = 'bold', textDecoration }: PropsWithChildren<Props>) => (
+  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.small} textDecoration={textDecoration}>
     {children}
   </S.HeadingElement>
 );
 
-const XSmall = ({ children, as = 'span', color, weight = 'bold' }: PropsWithChildren<Props>) => (
-  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.xsmall}>
+const XSmall = ({ children, as = 'span', color, weight = 'bold', textDecoration }: PropsWithChildren<Props>) => (
+  <S.HeadingElement as={as} color={color} weight={weights[weight]} size={sizes.xsmall} textDecoration={textDecoration}>
     {children}
   </S.HeadingElement>
 );
--- frontend/src/components/Text/Text.style.ts
+++ frontend/src/components/Text/Text.style.ts
@@ -4,10 +4,12 @@ interface Props {
   color: string;
   weight: number;
   size: string;
+  textDecoration?: 'none' | 'underline' | 'overline' | 'line-through';
 }
 
 export const TextElement = styled.span<Props>`
   font-size: ${({ size }) => size};
   font-weight: ${({ weight }) => weight};
   color: ${({ color }) => color};
+  text-decoration: ${({ textDecoration }) => textDecoration || 'none'};
 `;
