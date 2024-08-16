3c10c75cefda77714267754404cf39cac4be58d0
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-16 15:00:29 +0900

    fix(repository): default 메서드  MemberJpaRepository 로 이동

--- backend/src/main/java/codezap/member/repository/MemberJpaRepository.java
+++ backend/src/main/java/codezap/member/repository/MemberJpaRepository.java
@@ -1,5 +1,7 @@
 package codezap.member.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.http.HttpStatus;
 
@@ -13,6 +15,13 @@ public interface MemberJpaRepository extends MemberRepository, JpaRepository<Mem
         return findById(id).orElseThrow(
                 () -> new CodeZapException(HttpStatus.NOT_FOUND, "식별자 " + id + "에 해당하는 멤버가 존재하지 않습니다."));
     }
+
+    default Member fetchByLoginId(String loginId) {
+        return findByLoginId(loginId)
+                .orElseThrow(() -> new CodeZapException(HttpStatus.UNAUTHORIZED, "존재하지 않는 아이디 " + loginId + " 입니다."));
+    }
+
+    Optional<Member> findByLoginId(String loginId);
 
     boolean existsByLoginId(String loginId);
 }
--- backend/src/main/java/codezap/member/repository/MemberRepository.java
+++ backend/src/main/java/codezap/member/repository/MemberRepository.java
@@ -1,26 +1,16 @@
 package codezap.member.repository;
 
-import java.util.Optional;
-
-import org.springframework.http.HttpStatus;
-
-import codezap.global.exception.CodeZapException;
 import codezap.member.domain.Member;
 
 public interface MemberRepository {
 
     Member fetchById(Long id);
 
+    Member fetchByLoginId(String loginId);
+
     boolean existsByLoginId(String loginId);
 
     boolean existsById(Long id);
-
-    Optional<Member> findByLoginId(String loginId);
-
-    default Member fetchByLoginId(String loginId) {
-        return findByLoginId(loginId)
-                .orElseThrow(() -> new CodeZapException(HttpStatus.UNAUTHORIZED, "존재하지 않는 아이디 " + loginId + " 입니다."));
-    }
 
     Member save(Member member);
 }
