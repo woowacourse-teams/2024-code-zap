64367efe0a04fc1a87b1b1ebd8e3b8f8d2f1e406
jayming66 <mjw8941@gmail.com>
2024-08-17 00:54:54 +0900

    feat(types): svg 타입 설정

--- frontend/src/types/images.d.ts
+++ frontend/src/types/images.d.ts
@@ -2,3 +2,10 @@ declare module '*.png' {
   const value: string;
   export default value;
 }
+
+declare module '*.svg' {
+  import React = require('react');
+
+  const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+  export default ReactComponent;
+}
