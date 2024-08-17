4232d529792c3f743641af8482ccaedae05831f2
jayming66 <mjw8941@gmail.com>
2024-08-17 16:38:02 +0900

    refactor(images): 기존 pencil PNG 제거

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -1,6 +1,8 @@
+// Icon
+export { default as PencilIcon } from './pencil.svg';
+
 export { default as logoIcon } from './logo_24x24.png';
 export { default as newTemplateIcon } from './newTemplate.png';
-export { default as pencilIcon } from './pencil.png';
 export { default as searchIcon } from './search.png';
 export { default as trashcanIcon } from './trashcan.png';
 export { default as userMenuIcon } from './userMenu_38x38.png';
