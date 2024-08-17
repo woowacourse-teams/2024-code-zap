097489de7bb71b88439bd8b976a298c842d6ddc2
jayming66 <mjw8941@gmail.com>
2024-08-17 21:38:21 +0900

    refactor(images): passwordEye PNG -> Eye SVG로 변경

--- frontend/src/assets/images/index.ts
+++ frontend/src/assets/images/index.ts
@@ -6,8 +6,8 @@ export { default as TrashcanIcon } from './trashcan.svg';
 export { default as UserCircleIcon } from './userCircle.svg';
 export { default as ChevronIcon } from './chevron.svg';
 export { default as XCircleIcon } from './xCircle.svg';
+export { default as EyeIcon } from './eye.svg';
 
-export { default as passwordEyeIcon } from './passwordEye.png';
 export { default as codezapLogo } from './codezapLogo.png';
 export { default as tiger } from './tiger.png';
 export { default as arrowUp } from './arrowUp.png';
