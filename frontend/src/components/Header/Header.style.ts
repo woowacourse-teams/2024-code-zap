1925bbadea34b3b1d7dabdf8e36ec07a03ac4141
jayming66 <mjw8941@gmail.com>
2024-08-18 18:35:16 +0900

    design(Header): radius 제거

--- frontend/src/components/Header/Header.style.ts
+++ frontend/src/components/Header/Header.style.ts
@@ -60,7 +60,6 @@ export const HeaderMenu = styled.div<{ menuOpen: boolean }>`
     visibility: hidden;
     opacity: 0;
     background-color: white;
-    border-radius: 8px 0 0 8px;
 
     transition:
       transform 0.3s ease-in-out,
