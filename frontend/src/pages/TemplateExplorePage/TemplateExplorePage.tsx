77a23bf9f678e253b4788bd7c64326fd8fed0bb9
jayming66 <mjw8941@gmail.com>
2024-08-20 22:37:48 +0900

    feat(TemplateCard): 템플릿 카드 클릭 시, 해당 템플릿 단건 조회 페이지로 라우트

--- frontend/src/pages/TemplateExplorePage/TemplateExplorePage.tsx
+++ frontend/src/pages/TemplateExplorePage/TemplateExplorePage.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 import { Flex, Heading, TemplateCard } from '@/components';
 import { useWindowWidth } from '@/hooks/utils';
 import { useTemplateExploreQuery } from '@/queries/template';
@@ -9,12 +11,18 @@ const TemplateExplorePage = () => {
   const { data: templateData } = useTemplateExploreQuery({});
   const windowWidth = useWindowWidth();
 
+  const templates = templateData?.templates || [];
+
   return (
-    <Flex direction='column' gap='3rem' align='center' css={{ paddingTop: '10rem' }}>
+    <Flex direction='column' gap='4rem' align='flex-start' css={{ paddingTop: '5rem' }}>
       <Heading.Large color='black'>구경가기 페이지입니다.</Heading.Large>
 
       <S.TemplateExplorePageContainer cols={getGridCols(windowWidth)}>
-        {templateData?.templates.map((template) => <TemplateCard key={template.id} template={template} />)}
+        {templates.map((template) => (
+          <Link to={`/templates/${template.id}`} key={template.id}>
+            <TemplateCard template={template} />
+          </Link>
+        ))}
       </S.TemplateExplorePageContainer>
     </Flex>
   );
