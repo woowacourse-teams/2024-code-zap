9cde6fc2168164ae1610ed749d73645f4b1a2b66
월하 <cerezo00@naver.com>
2024-08-20 11:48:03 +0900

    feat(components): Text, Heading 컴포넌트에 textDecoration 속성 추가

--- frontend/src/components/Text/Text.tsx
+++ frontend/src/components/Text/Text.tsx
@@ -8,38 +8,41 @@ type As = 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'p' | 'span';
 const weights = { ...theme.font.weight };
 const sizes = { ...theme.font.size.text };
 
+type TextDecoration = 'none' | 'underline' | 'overline' | 'line-through';
+
 interface Props {
   as?: As;
   color: string;
   weight?: keyof typeof weights;
+  textDecoration?: TextDecoration;
 }
 
-const XLarge = ({ children, as = 'span', color, weight = 'regular' }: PropsWithChildren<Props>) => (
-  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.xlarge}>
+const XLarge = ({ children, as = 'span', color, weight = 'regular', textDecoration }: PropsWithChildren<Props>) => (
+  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.xlarge} textDecoration={textDecoration}>
     {children}
   </S.TextElement>
 );
 
-const Large = ({ children, as = 'span', color, weight = 'regular' }: PropsWithChildren<Props>) => (
-  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.large}>
+const Large = ({ children, as = 'span', color, weight = 'regular', textDecoration }: PropsWithChildren<Props>) => (
+  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.large} textDecoration={textDecoration}>
     {children}
   </S.TextElement>
 );
 
-const Medium = ({ children, as = 'span', color, weight = 'regular' }: PropsWithChildren<Props>) => (
-  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.medium}>
+const Medium = ({ children, as = 'span', color, weight = 'regular', textDecoration }: PropsWithChildren<Props>) => (
+  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.medium} textDecoration={textDecoration}>
     {children}
   </S.TextElement>
 );
 
-const Small = ({ children, as = 'span', color, weight = 'regular' }: PropsWithChildren<Props>) => (
-  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.small}>
+const Small = ({ children, as = 'span', color, weight = 'regular', textDecoration }: PropsWithChildren<Props>) => (
+  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.small} textDecoration={textDecoration}>
     {children}
   </S.TextElement>
 );
 
-const XSmall = ({ children, as = 'span', color, weight = 'regular' }: PropsWithChildren<Props>) => (
-  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.xsmall}>
+const XSmall = ({ children, as = 'span', color, weight = 'regular', textDecoration }: PropsWithChildren<Props>) => (
+  <S.TextElement as={as} color={color} weight={weights[weight]} size={sizes.xsmall} textDecoration={textDecoration}>
     {children}
   </S.TextElement>
 );
