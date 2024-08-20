7254ba59025f57793b346e808b1bfdf4d7b942bc
jayming66 <mjw8941@gmail.com>
2024-08-20 22:38:12 +0900

    feat(TempalteExplorePage): 정렬 드롭다운 추가

--- frontend/src/pages/TemplateExplorePage/TemplateExplorePage.tsx
+++ frontend/src/pages/TemplateExplorePage/TemplateExplorePage.tsx
@@ -1,14 +1,16 @@
 import { Link } from 'react-router-dom';
 
-import { Flex, Heading, TemplateCard } from '@/components';
-import { useWindowWidth } from '@/hooks/utils';
+import { DEFAULT_SORTING_OPTION, SORTING_OPTIONS } from '@/api';
+import { Dropdown, Flex, Heading, TemplateCard } from '@/components';
+import { useDropdown, useWindowWidth } from '@/hooks/utils';
 import { useTemplateExploreQuery } from '@/queries/template';
 import * as S from './TemplateExplorePage.style';
 
 const getGridCols = (windowWidth: number) => (windowWidth <= 1024 ? 1 : 2);
 
 const TemplateExplorePage = () => {
-  const { data: templateData } = useTemplateExploreQuery({});
+  const { currentValue: sortingOption, ...dropdownProps } = useDropdown(DEFAULT_SORTING_OPTION);
+  const { data: templateData } = useTemplateExploreQuery({ sort: sortingOption.key });
   const windowWidth = useWindowWidth();
 
   const templates = templateData?.templates || [];
@@ -16,6 +18,15 @@ const TemplateExplorePage = () => {
   return (
     <Flex direction='column' gap='4rem' align='flex-start' css={{ paddingTop: '5rem' }}>
       <Heading.Large color='black'>구경가기 페이지입니다.</Heading.Large>
+      <Flex width='100%' justify='flex-end'>
+        {' '}
+        <Dropdown
+          {...dropdownProps}
+          options={SORTING_OPTIONS}
+          currentValue={sortingOption}
+          getOptionLabel={(option) => option.value}
+        />
+      </Flex>
 
       <S.TemplateExplorePageContainer cols={getGridCols(windowWidth)}>
         {templates.map((template) => (
