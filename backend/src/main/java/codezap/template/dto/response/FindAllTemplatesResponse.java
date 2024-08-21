ada49dfc41ed86aef504a10c588df7c9e1bf9dbc
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-08-21 13:24:01 +0900

    docs(controller): 템플릿 조회 시 작성자 정보 포함하도록 변경

--- backend/src/main/java/codezap/template/dto/response/FindAllTemplatesResponse.java
+++ backend/src/main/java/codezap/template/dto/response/FindAllTemplatesResponse.java
@@ -19,22 +19,32 @@ public record FindAllTemplatesResponse(
     public record ItemResponse(
             @Schema(description = "템플릿 ID", example = "0")
             Long id,
+
+            @Schema(description = "회원 설명")
+            FindMemberResponse member,
+
             @Schema(description = "템플릿명", example = "스프링 로그인 구현")
             String title,
+
             @Schema(description = "템플릿 설명", example = "Jwt 토큰을 이용하여 로그인 기능을 구현합니다.")
             String description,
+
             @Schema(description = "태그 목록")
             List<FindTagResponse> tags,
+
             @Schema(description = "썸네일")
             FindThumbnailResponse thumbnail,
+
             @Schema(description = "템플릿 생성 시간", example = "2024-11-10 12:00:00", type = "string")
             LocalDateTime createdAt,
+
             @Schema(description = "템플릿 수정 시간", example = "2024-11-11 12:00:00", type = "string")
             LocalDateTime modifiedAt
     ) {
         public static ItemResponse of(Template template, List<Tag> templateTags, SourceCode thumbnailSourceCode) {
             return new ItemResponse(
                     template.getId(),
+                    null,
                     template.getTitle(),
                     template.getDescription(),
                     templateTags.stream()
