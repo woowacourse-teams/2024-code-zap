503bb4e86144ea975af455ae59c427cf78d5e2b2
월하 <cerezo00@naver.com>
2024-08-21 12:07:12 +0900

    refactor(src): TagFilterMenu 디자인 리팩토링

--- frontend/src/components/TagFilterMenu/TagFilterMenu.style.ts
+++ frontend/src/components/TagFilterMenu/TagFilterMenu.style.ts
@@ -4,13 +4,38 @@ import { theme } from '@/style/theme';
 
 export const TagFilterMenuContainer = styled.div`
   display: flex;
-  flex-wrap: wrap;
-  gap: 0.5rem;
+  gap: 1rem;
   align-items: flex-start;
 
-  width: 12.5rem;
+  width: 100%;
   padding: 1rem;
 
   border: 1px solid ${theme.color.light.secondary_300};
   border-radius: 8px;
+`;
+
+export const TagButtonsContainer = styled.div<{ height: string }>`
+  overflow: hidden;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 0.5rem;
+  align-items: flex-start;
+
+  width: 100%;
+  height: ${({ height }) => height};
+
+  transition: height 0.3s ease-in-out;
+`;
+
+export const ShowMoreButton = styled.button<{ isExpanded: boolean }>`
+  cursor: pointer;
+  width: 1.875rem;
+  height: 1.875rem;
+  transition: transform 0.3s ease-in-out;
+
+  ${({ isExpanded }) =>
+    isExpanded &&
+    `
+      transform: rotate(180deg);
+    `}
 `;
