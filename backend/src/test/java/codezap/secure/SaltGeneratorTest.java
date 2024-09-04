512a34a6e3eb9e466d7052eb0875cfa8c7f65b16
zangsu <zangsu_@naver.com>
2024-09-04 17:21:56 +0900

    test: 난수 생성 테스트 무시

--- backend/src/test/java/codezap/secure/SaltGeneratorTest.java
+++ backend/src/test/java/codezap/secure/SaltGeneratorTest.java
@@ -5,6 +5,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -14,6 +15,7 @@ class SaltGeneratorTest {
 
     @Test
     @DisplayName("1,000,000번의 난수 생성에서 난수 충돌이 발생하지 않는다.")
+    @Disabled
     void test() {
         Set<String> salts = new HashSet<>();
         for (int i = 0; i < 1_000_000; i++) {
