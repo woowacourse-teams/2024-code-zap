e8b3e0c8117ecfcf860f77f8d5a75e8080627705
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-09-05 13:54:50 +0900

    refactor(WebCorsConfiguration): cors를 허용하는 origin 지정

--- backend/src/main/java/codezap/global/cors/WebCorsConfiguration.java
+++ backend/src/main/java/codezap/global/cors/WebCorsConfiguration.java
@@ -10,7 +10,7 @@ public class WebCorsConfiguration implements WebMvcConfigurer {
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowCredentials(true)
-                .allowedOriginPatterns("*")
+                .allowedOriginPatterns("https://*.codezap.com", "http://localhost:8080")
                 .allowedMethods("*")
                 .exposedHeaders("*");
     }
