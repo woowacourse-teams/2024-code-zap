33185a11b297717af9c8f6eb6d518543b12c0079
zangsu <zangsu_@naver.com>
2024-09-05 11:24:40 +0900

    test: 테스트 메서드 이름을 직관적으로 변경

--- backend/src/test/java/codezap/secure/SaltGeneratorTest.java
+++ backend/src/test/java/codezap/secure/SaltGeneratorTest.java
@@ -16,7 +16,7 @@ class RandomSaltGeneratorTest {
     @Test
     @DisplayName("1,000,000번의 난수 생성에서 난수 충돌이 발생하지 않는다.")
     @Disabled
-    void test() {
+    void randomGeneratorCollisionTest() {
         Set<String> salts = new HashSet<>();
         for (int i = 0; i < 1_000_000; i++) {
             salts.add(saltGenerator.getSalt());
