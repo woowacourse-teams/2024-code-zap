c6e7b60c9052e23ee56747a1715b59c6386ab3f9
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-10 09:55:44 +0900

    test: TagJpaRepository 테스트 수정

--- backend/src/test/java/codezap/member/repository/MemberJpaRepositoryTest.java
+++ backend/src/test/java/codezap/member/repository/MemberJpaRepositoryTest.java
@@ -3,16 +3,14 @@ package codezap.member.repository;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
-import java.util.Optional;
-
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.JpaRepository;
 
 import codezap.category.domain.Category;
 import codezap.category.repository.CategoryJpaRepository;
+import codezap.category.repository.CategoryRepository;
 import codezap.fixture.CategoryFixture;
 import codezap.fixture.MemberFixture;
 import codezap.global.exception.CodeZapException;
@@ -20,23 +18,24 @@ import codezap.global.repository.JpaRepositoryTest;
 import codezap.member.domain.Member;
 import codezap.template.domain.Template;
 import codezap.template.repository.TemplateJpaRepository;
+import codezap.template.repository.TemplateRepository;
 
 @JpaRepositoryTest
 class MemberJpaRepositoryTest {
 
-    private final MemberJpaRepository memberJpaRepository;
-    private final TemplateJpaRepository templateJpaRepository;
-    private final CategoryJpaRepository categoryJpaRepository;
+    private final MemberRepository memberRepository;
+    private final TemplateRepository templateRepository;
+    private final CategoryRepository categoryRepository;
 
     @Autowired
     MemberJpaRepositoryTest(
-            MemberJpaRepository memberJpaRepository,
-            TemplateJpaRepository templateJpaRepository,
-            CategoryJpaRepository categoryJpaRepository
+            MemberJpaRepository memberRepository,
+            TemplateJpaRepository templateRepository,
+            CategoryJpaRepository categoryRepository
     ) {
-        this.memberJpaRepository = memberJpaRepository;
-        this.templateJpaRepository = templateJpaRepository;
-        this.categoryJpaRepository = categoryJpaRepository;
+        this.memberRepository = memberRepository;
+        this.templateRepository = templateRepository;
+        this.categoryRepository = categoryRepository;
     }
 
     @Nested
@@ -46,9 +45,9 @@ class MemberJpaRepositoryTest {
         @DisplayName("성공 : id로 Member를 알아낼 수 있다.")
         void fetchByIdSuccess() {
             Member member = MemberFixture.getFirstMember();
-            member = ((JpaRepository<Member, Long>) memberJpaRepository).save(member);
+            member = memberRepository.save(member);
 
-            Member actual = memberJpaRepository.fetchById(member.getId());
+            Member actual = memberRepository.fetchById(member.getId());
 
             assertThat(actual).isEqualTo(member);
         }
@@ -58,7 +57,7 @@ class MemberJpaRepositoryTest {
         void fetchByIdFailByNotExistsId() {
             long id = 100;
 
-            assertThatThrownBy(() -> memberJpaRepository.fetchById(id))
+            assertThatThrownBy(() -> memberRepository.fetchById(id))
                     .isInstanceOf(CodeZapException.class)
                     .hasMessage("식별자 " + id + "에 해당하는 멤버가 존재하지 않습니다.");
         }
@@ -71,9 +70,9 @@ class MemberJpaRepositoryTest {
         @DisplayName("성공 : 아이디로 Member를 알아낼 수 있다.")
         void fetchByNameSuccess() {
             Member member = MemberFixture.getFirstMember();
-            member = ((JpaRepository<Member, Long>) memberJpaRepository).save(member);
+            member = memberRepository.save(member);
 
-            Member actual = memberJpaRepository.fetchByName(member.getName());
+            Member actual = memberRepository.fetchByName(member.getName());
 
             assertThat(actual).isEqualTo(member);
         }
@@ -83,32 +82,9 @@ class MemberJpaRepositoryTest {
         void fetchByNameFailByNotExistsId() {
             String name = "켬미";
 
-            assertThatThrownBy(() -> memberJpaRepository.fetchByName(name))
+            assertThatThrownBy(() -> memberRepository.fetchByName(name))
                     .isInstanceOf(CodeZapException.class)
                     .hasMessage("존재하지 않는 아이디 " + name + " 입니다.");
-        }
-    }
-
-    @Nested
-    @DisplayName("아이디로 Member 조회")
-    class findByName {
-        @Test
-        @DisplayName("성공 : 아이디로 Member를 알아낼 수 있다.")
-        void findByNameSuccess() {
-            Member member = MemberFixture.getFirstMember();
-            member = ((JpaRepository<Member, Long>) memberJpaRepository).save(member);
-
-            Optional<Member> actual = memberJpaRepository.findByName(member.getName());
-
-            assertThat(actual).isEqualTo(Optional.of(member));
-        }
-
-        @Test
-        @DisplayName("실패 : 존재하지 않는 아이디인 경우 optional 값이 반환된다.")
-        void findByNameFailByNotExistsId() {
-            Optional<Member> actual = memberJpaRepository.findByName("kyummi");
-
-            assertThat(actual).isEmpty();
         }
     }
 
@@ -118,13 +94,12 @@ class MemberJpaRepositoryTest {
         @Test
         @DisplayName("성공 : 템플릿 id로 Member를 알아낼 수 있다.")
         void fetchByTemplateIdSuccess() {
-            Member member = ((JpaRepository<Member, Long>) memberJpaRepository).save(MemberFixture.getFirstMember());
-            Category category = ((JpaRepository<Category, Long>) categoryJpaRepository).save(
-                    CategoryFixture.getFirstCategory());
+            Member member = memberRepository.save(MemberFixture.getFirstMember());
+            Category category = categoryRepository.save(CategoryFixture.getFirstCategory());
             Template template = new Template(member, "title", "description", category);
-            template = ((JpaRepository<Template, Long>) templateJpaRepository).save(template);
+            template = templateRepository.save(template);
 
-            Member actual = memberJpaRepository.fetchByTemplateId(template.getId());
+            Member actual = memberRepository.fetchByTemplateId(template.getId());
 
             assertThat(actual).isEqualTo(member);
         }
@@ -134,35 +109,9 @@ class MemberJpaRepositoryTest {
         void fetchByTemplateIdFailByNotExistsId() {
             long id = 100;
 
-            assertThatThrownBy(() -> memberJpaRepository.fetchByTemplateId(id))
+            assertThatThrownBy(() -> memberRepository.fetchByTemplateId(id))
                     .isInstanceOf(CodeZapException.class)
                     .hasMessage("템플릿에 대한 멤버가 존재하지 않습니다.");
-        }
-    }
-
-    @Nested
-    @DisplayName("템플릿 id로 Member 조회")
-    class findByTemplateId {
-        @Test
-        @DisplayName("성공 : 템플릿 id로 Member를 알아낼 수 있다.")
-        void findByTemplateIdSuccess() {
-            Member member = ((JpaRepository<Member, Long>) memberJpaRepository).save(MemberFixture.getFirstMember());
-            Category category = ((JpaRepository<Category, Long>) categoryJpaRepository).save(
-                    CategoryFixture.getFirstCategory());
-            Template template = new Template(member, "title", "description", category);
-            template = ((JpaRepository<Template, Long>) templateJpaRepository).save(template);
-
-            Optional<Member> actual = memberJpaRepository.findByTemplateId(template.getId());
-
-            assertThat(actual).isEqualTo(Optional.of(member));
-        }
-
-        @Test
-        @DisplayName("실패 : 존재하지 않는 템플릿 id인 경우 optional 값이 반환된다.")
-        void findByTemplateIdFailByNotExistsId() {
-            Optional<Member> actual = memberJpaRepository.findByTemplateId(100L);
-
-            assertThat(actual).isEmpty();
         }
     }
 
@@ -172,9 +121,9 @@ class MemberJpaRepositoryTest {
         @Test
         @DisplayName("성공 : 해당 아이디가 존재하면 true를 반환한다.")
         void existsByNameReturnTrue() {
-            Member member = ((JpaRepository<Member, Long>) memberJpaRepository).save(MemberFixture.getFirstMember());
+            Member member = memberRepository.save(MemberFixture.getFirstMember());
 
-            boolean actual = memberJpaRepository.existsByName(member.getName());
+            boolean actual = memberRepository.existsByName(member.getName());
 
             assertThat(actual).isTrue();
         }
@@ -182,7 +131,7 @@ class MemberJpaRepositoryTest {
         @Test
         @DisplayName("성공 : 해당 아이디가 존재하면 false를 반환한다.")
         void existsByNameReturnFalse() {
-            boolean actual = memberJpaRepository.existsByName("kkk");
+            boolean actual = memberRepository.existsByName("kkk");
 
             assertThat(actual).isFalse();
         }
