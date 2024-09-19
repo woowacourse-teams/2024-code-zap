0c0fb12bca5b3788beb7393207b053fe3f875418
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-20 02:04:30 +0900

    test: 변수 추출 제거 및 DisplayName 변경

--- backend/src/test/java/codezap/category/service/facade/MemberCategoryTemplateApplicationServiceTest.java
+++ backend/src/test/java/codezap/category/service/facade/MemberCategoryTemplateApplicationServiceTest.java
@@ -43,75 +43,67 @@ class MemberCategoryTemplateApplicationServiceTest {
         @DisplayName("카테고리 아이디로 카테고리 삭제 성공")
         void deleteByIdSuccess() {
             Member member = memberRepository.save(MemberFixture.memberFixture());
-            MemberDto memberDto = MemberDto.from(member);
             Category category = categoryRepository.save(new Category("카테고리 1", member));
             Long categoryId = category.getId();
 
-            sut.deleteById(memberDto, categoryId);
+            sut.deleteById(MemberDto.from(member), categoryId);
 
             boolean actual = categoryRepository.existsById(categoryId);
             assertThat(actual).isFalse();
         }
 
         @Test
-        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 멤버 존재하지 않음")
-        void deleteByIdFailNotExistsMember() {
+        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 카테고리 존재하지 않음")
+        void deleteByIdFailNotExistsCategory() {
             Member member = memberRepository.save(MemberFixture.memberFixture());
-            MemberDto memberDto = MemberDto.from(member);
             Long notExistsId = 100L;
 
-            assertThatThrownBy(() -> sut.deleteById(memberDto, notExistsId))
+            assertThatThrownBy(() -> sut.deleteById(MemberDto.from(member), notExistsId))
                     .isInstanceOf(CodeZapException.class)
                     .hasMessage("식별자 " + notExistsId + "에 해당하는 카테고리가 존재하지 않습니다.");
         }
 
         @Test
-        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 카테고리 존재하지 않음")
-        void deleteByIdFailNotExistsCategory() {
-            MemberDto memberDto = MemberDto.from(MemberFixture.memberFixture());
+        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 멤버 존재하지 않음")
+        void deleteByIdFailNotExistsMember() {
+            MemberDto notExistsMemberDto = MemberDto.from(MemberFixture.memberFixture());
 
-            assertThatThrownBy(() -> sut.deleteById(memberDto, 1L))
+            assertThatThrownBy(() -> sut.deleteById(notExistsMemberDto, 1L))
                     .isInstanceOf(CodeZapException.class)
-                    .hasMessage("식별자 " + memberDto.id() + "에 해당하는 멤버가 존재하지 않습니다.");
+                    .hasMessage("식별자 " + notExistsMemberDto.id() + "에 해당하는 멤버가 존재하지 않습니다.");
         }
 
         @Test
         @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 카테고리를 삭제할 권한이 없음")
         void deleteByIdFailNoPermission() {
             Member member = memberRepository.save(MemberFixture.memberFixture());
-            MemberDto memberDto = MemberDto.from(member);
             Member otherMember = memberRepository.save(MemberFixture.createFixture("켬미"));
             Category category = categoryRepository.save(new Category("카테고리 1", otherMember));
-            Long categoryId = category.getId();
 
-            assertThatThrownBy(() -> sut.deleteById(memberDto, categoryId))
+            assertThatThrownBy(() -> sut.deleteById(MemberDto.from(member), category.getId()))
                     .isInstanceOf(CodeZapException.class)
                     .hasMessage("해당 카테고리를 수정 또는 삭제할 권한이 없는 유저입니다.");
         }
 
         @Test
-        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 템플릿 존재")
+        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 해당 카테고리에 속한 템플릿이 존재하면 삭제할 수 없음")
         void deleteByIdFailExistsTemplate() {
             Member member = memberRepository.save(MemberFixture.memberFixture());
-            MemberDto memberDto = MemberDto.from(member);
             Category category = categoryRepository.save(new Category("카테고리 1", member));
             templateRepository.save(new Template(member, "title", "desciption", category));
-            Long categoryId = category.getId();
 
-            assertThatThrownBy(() -> sut.deleteById(memberDto, categoryId))
+            assertThatThrownBy(() -> sut.deleteById(MemberDto.from(member), category.getId()))
                     .isInstanceOf(CodeZapException.class)
                     .hasMessage("템플릿이 존재하는 카테고리는 삭제할 수 없습니다.");
         }
 
         @Test
-        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 기본 카테고리")
+        @DisplayName("카테고리 아이디로 카테고리 삭제 실패 : 기본 카테고리는 삭제할 수 없음")
         void deleteByIdFailDefaultCategory() {
             Member member = memberRepository.save(MemberFixture.memberFixture());
-            MemberDto memberDto = MemberDto.from(member);
-            Category category = categoryRepository.save(Category.createDefaultCategory(member));
-            Long categoryId = category.getId();
+            Category defaultCategory = categoryRepository.save(Category.createDefaultCategory(member));
 
-            assertThatThrownBy(() -> sut.deleteById(memberDto, categoryId))
+            assertThatThrownBy(() -> sut.deleteById(MemberDto.from(member), defaultCategory.getId()))
                     .isInstanceOf(CodeZapException.class)
                     .hasMessage("기본 카테고리는 삭제할 수 없습니다.");
         }
