262d293419112de356d932188cd27622246b7535
헤인 <hain7ce@gmail.com>
2024-09-20 18:59:12 +0900

    refactor(tests): search 테스트에서 검색 테스트의 키워드 '테스트'=>'검색테스트'로 변경

--- frontend/playwright/tests/search.spec.ts
+++ frontend/playwright/tests/search.spec.ts
@@ -3,15 +3,17 @@ import { test, expect } from '@playwright/test';
 import { searchTemplates } from './search.actions';
 import { waitForSuccess } from './utils';
 
-test('검색창에 `테스트`를 입력하면 `테스트`가 내용에 포함된 템플릿 목록을 확인할 수 있다.', async ({ page }) => {
+test('검색창에 `검색테스트`를 입력하면 `검색테스트`가 내용에 포함된 템플릿 목록을 확인할 수 있다.', async ({
+  page,
+}) => {
   await page.goto('/my-templates');
 
-  const keyword = '테스트';
+  const keyword = '검색테스트';
 
   await searchTemplates({ page, keyword });
 
   await waitForSuccess({ page, apiUrl: '/templates?keyword' });
-  await expect(page.getByRole('link', { name: /테스트/ })).toBeVisible();
+  await expect(page.getByRole('link', { name: /검색테스트/ })).toBeVisible();
 });
 
 test('검색창에 `ㅁㅅㅌㅇ`를 입력할 경우 `검색 결과가 없습니다`가 나온다.', async ({ page }) => {
