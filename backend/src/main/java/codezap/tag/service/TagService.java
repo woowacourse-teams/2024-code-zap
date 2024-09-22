ce71cc4b519a78b407fe1d193531a2f3320fd340
재우 <Akaps6768@gmail.com>
2024-09-23 00:14:33 +0900

    refactor(service): 사용하지 않는 메서드 삭제

--- backend/src/main/java/codezap/tag/service/TagService.java
+++ backend/src/main/java/codezap/tag/service/TagService.java
@@ -2,11 +2,9 @@ package codezap.tag.service;
 
 import java.util.List;
 
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import codezap.global.exception.CodeZapException;
 import codezap.tag.domain.Tag;
 import codezap.tag.dto.response.FindAllTagsResponse;
 import codezap.tag.dto.response.FindTagResponse;
@@ -50,20 +48,6 @@ public class TagService {
         return templateTagRepository.findAllByTemplate(template).stream()
                 .map(TemplateTag::getTag)
                 .toList();
-    }
-
-    public List<Long> getTemplateIdContainTagIds(List<Long> tagIds) {
-        if (tagIds.isEmpty()) {
-            throw new CodeZapException(HttpStatus.BAD_REQUEST, "태그 ID가 0개입니다. 필터링 하지 않을 경우 null로 전달해주세요.");
-        }
-        tagIds.forEach(this::validateTagId);
-        return templateTagRepository.findAllTemplateIdInTagIds(tagIds, tagIds.size());
-    }
-
-    private void validateTagId(Long tagId) {
-        if (!tagRepository.existsById(tagId)) {
-            throw new CodeZapException(HttpStatus.NOT_FOUND, "식별자 " + tagId + "에 해당하는 태그가 존재하지 않습니다.");
-        }
     }
 
     public FindAllTagsResponse findAllByTemplates(List<Template> templates) {
