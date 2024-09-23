9f4dd550ea4245c86e2be402129bb2202c3e579e
zangsu <zangsu_@naver.com>
2024-09-23 23:45:25 +0900

    refactor: 필요없는 어노테이션 제거

--- backend/src/main/java/codezap/likes/repository/LikesJpaRepository.java
+++ backend/src/main/java/codezap/likes/repository/LikesJpaRepository.java
@@ -1,10 +1,8 @@
 package codezap.likes.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import codezap.likes.domain.Likes;
 
-@Repository
 public interface LikesJpaRepository extends LikesRepository, JpaRepository<Likes, Long> {
 }
