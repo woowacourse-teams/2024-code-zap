24bd1649b53062bf1a9d66ebd77f41165575cf6b
zangsu <zangsu_@naver.com>
2024-09-23 23:38:32 +0900

    feat: flyway 파일 추가

---
+++ backend/src/main/resources/db/migration/V3__add_likes.sql
@@ -0,0 +1,16 @@
+-- Create tables
+CREATE TABLE likes
+(
+    created_at  DATETIME(6) NOT NULL,
+    id          BIGINT      NOT NULL AUTO_INCREMENT,
+    member_id   BIGINT      NOT NULL,
+    modified_at DATETIME(6) NOT NULL,
+    template_id BIGINT      NOT NULL,
+    PRIMARY KEY (id)
+) ENGINE=InnoDB
+
+-- Add foreign key constraints
+ALTER TABLE likes
+    ADD CONSTRAINT FKa4vkf1skcfu5r6o5gfb5jf295 FOREIGN KEY (member_id) REFERENCES member (id)
+ALTER TABLE likes
+    ADD CONSTRAINT FKbrgthyu05fsswb9ysi2l79g7r FOREIGN KEY (template_id) REFERENCES template (id)
