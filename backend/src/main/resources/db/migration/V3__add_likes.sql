93f0a59881a110ce193426e2cec95a8ec8999b94
zangsu <zangsu_@naver.com>
2024-09-24 14:14:36 +0900

    refactor: SQL 문법 수정
    - ; 추가

--- backend/src/main/resources/db/migration/V3__add_likes.sql
+++ backend/src/main/resources/db/migration/V3__add_likes.sql
@@ -7,10 +7,10 @@ CREATE TABLE likes
     modified_at DATETIME(6) NOT NULL,
     template_id BIGINT      NOT NULL,
     PRIMARY KEY (id)
-) ENGINE=InnoDB
+) ENGINE=InnoDB;
 
 -- Add foreign key constraints
 ALTER TABLE likes
-    ADD CONSTRAINT FKa4vkf1skcfu5r6o5gfb5jf295 FOREIGN KEY (member_id) REFERENCES member (id)
+    ADD CONSTRAINT FKa4vkf1skcfu5r6o5gfb5jf295 FOREIGN KEY (member_id) REFERENCES member (id);
 ALTER TABLE likes
-    ADD CONSTRAINT FKbrgthyu05fsswb9ysi2l79g7r FOREIGN KEY (template_id) REFERENCES template (id)
+    ADD CONSTRAINT FKbrgthyu05fsswb9ysi2l79g7r FOREIGN KEY (template_id) REFERENCES template (id);
