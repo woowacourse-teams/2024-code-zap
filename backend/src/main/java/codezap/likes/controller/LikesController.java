dbe7dcb5623ccdbfd77a73f8e293e87f687ae55a
zangsu <zangsu_@naver.com>
2024-09-24 12:00:42 +0900

    refactor: 로그인 정보를 가져오기 위한 어노테이션 추가

--- backend/src/main/java/codezap/likes/controller/LikesController.java
+++ backend/src/main/java/codezap/likes/controller/LikesController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import codezap.auth.configuration.AuthenticationPrinciple;
 import codezap.likes.service.LikesService;
 import codezap.member.dto.MemberDto;
 import lombok.RequiredArgsConstructor;
@@ -17,13 +18,13 @@ public class LikesController implements SpringDocsLikesController {
     private final LikesService likesService;
 
     @PostMapping("like/{templateId}")
-    public ResponseEntity<Void> like(MemberDto memberDto, @PathVariable long templateId) {
+    public ResponseEntity<Void> like(@AuthenticationPrinciple MemberDto memberDto, @PathVariable long templateId) {
         likesService.like(memberDto, templateId);
         return ResponseEntity.ok().build();
     }
 
     @DeleteMapping("like/{templateId}")
-    public ResponseEntity<Void> cancelLike(MemberDto memberDto, @PathVariable long templateId) {
+    public ResponseEntity<Void> cancelLike(@AuthenticationPrinciple MemberDto memberDto, @PathVariable long templateId) {
         likesService.cancelLike(memberDto, templateId);
         return ResponseEntity.noContent().build();
     }
