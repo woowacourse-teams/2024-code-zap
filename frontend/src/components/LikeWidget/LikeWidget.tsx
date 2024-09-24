bedc9b7bd177e9f3d588cba1e4f63302e30ec1f5
월하 <cerezo00@naver.com>
2024-09-24 12:32:52 +0900

    refactor(LikeWidget): console.log 삭제

--- frontend/src/components/LikeWidget/LikeWidget.tsx
+++ frontend/src/components/LikeWidget/LikeWidget.tsx
@@ -12,15 +12,11 @@ interface Props {
   onLikeWidgetClick?: () => void;
 }
 
-const LikeWidget = ({ likeCount, isLiked, clickable = false, onLikeWidgetClick }: Props) => {
-  console.log('temp');
-
-  return (
-    <S.LikeButtonWidgetContainer isLiked={isLiked} clickable={clickable} onClick={onLikeWidgetClick}>
-      <LikeIcon state={isLiked ? 'like' : clickable ? 'unlike' : 'unClickable'} size={14} />
-      <Text.Small color={theme.color.light.secondary_800}>{formatWithK(likeCount)}</Text.Small>
-    </S.LikeButtonWidgetContainer>
-  );
-};
+const LikeWidget = ({ likeCount, isLiked, clickable = false, onLikeWidgetClick }: Props) => (
+  <S.LikeButtonWidgetContainer isLiked={isLiked} clickable={clickable} onClick={onLikeWidgetClick}>
+    <LikeIcon state={isLiked ? 'like' : clickable ? 'unlike' : 'unClickable'} size={14} />
+    <Text.Small color={theme.color.light.secondary_800}>{formatWithK(likeCount)}</Text.Small>
+  </S.LikeButtonWidgetContainer>
+);
 
 export default LikeWidget;
