474b51796a1d379510f1e2b450db25cd27a2148a
zangsu <zangsu_@naver.com>
2024-09-24 14:32:46 +0900

    refactor: 주석 제거

--- backend/src/test/java/codezap/likes/repository/FakeLikeRepository.java
+++ backend/src/test/java/codezap/likes/repository/FakeLikeRepository.java
@@ -4,7 +4,6 @@ import codezap.likes.domain.Likes;
 import codezap.member.domain.Member;
 import codezap.template.domain.Template;
 
-//기존의 Fake repo 사용 코드가 터지지 않도록 임시로 구현하는 코드
 public class FakeLikeRepository implements LikesRepository {
     @Override
     public Likes save(Likes likes) {
