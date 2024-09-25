116d8c1461485f12742240e880c4b48526682c3e
월하 <cerezo00@naver.com>
2024-09-25 18:14:10 +0900

    feat(NonmemberAlerter): NonmemberAlerter 컴포넌트 구현

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -8,6 +8,7 @@ export { default as Layout } from './Layout/Layout';
 export { default as LikeButton } from './LikeButton/LikeButton';
 export { default as LikeCounter } from './LikeCounter/LikeCounter';
 export { default as Modal } from './Modal/Modal';
+export { default as NonmemberAlerter } from './NonmemberAlerter/NonmemberAlerter';
 export { default as PagingButtons } from './PagingButtons/PagingButtons';
 export { default as SelectList } from './SelectList/SelectList';
 export { default as SourceCodeEditor } from './SourceCodeEditor/SourceCodeEditor';
