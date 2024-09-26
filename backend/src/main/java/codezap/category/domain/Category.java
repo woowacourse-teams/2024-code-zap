9c449365cced7fc81a77dbb17a3174ac1c4fa186
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-09-27 03:10:01 +0900

    refactor(domain): FetchType.LAZY 설정 추가

--- backend/src/main/java/codezap/category/domain/Category.java
+++ backend/src/main/java/codezap/category/domain/Category.java
@@ -2,6 +2,7 @@ package codezap.category.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -46,7 +47,7 @@ public class Category extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne(optional = false)
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
     private Member member;
 
     @Column(nullable = false)
