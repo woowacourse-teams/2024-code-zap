469dae9c0c3016a7ee76931f8775b0e9f5ffb7ab
vi-wolhwa <cerezo00@naver.com>
2024-09-27 03:44:17 +0900

    chore: 번들 brotli 압축

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,6 +1,6 @@
 const { merge } = require('webpack-merge');
 const Dotenv = require('dotenv-webpack');
-
+const CompressionPlugin = require('compression-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = () => {
@@ -13,6 +13,10 @@ module.exports = () => {
         systemvars: true,
         safe: true,
         ignoreStub: true,
+      }),
+      new CompressionPlugin({
+        algorithm: 'brotliCompress',
+        exclude: /\.(png|jpg|gif|webp|webm|map|ico)$/i,
       }),
     ],
   });
