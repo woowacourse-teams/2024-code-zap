58baebd4b518a7ba513490d49e012f10470f0553
재우 <Akaps6768@gmail.com>
2024-09-26 11:24:54 +0900

    refactor(category): 예외메시지 통일

--- backend/src/main/java/codezap/category/domain/Category.java
+++ backend/src/main/java/codezap/category/domain/Category.java
@@ -68,7 +68,7 @@ public class Category extends BaseTimeEntity {
 
     public void validateAuthorization(Member member) {
         if (!getMember().equals(member)) {
-            throw new CodeZapException(HttpStatus.UNAUTHORIZED, "해당 카테고리에 대한 권한이 없습니다.");
+            throw new CodeZapException(HttpStatus.UNAUTHORIZED, "해당 카테고리를 수정 또는 삭제할 권한이 없는 유저입니다.");
         }
     }
 
