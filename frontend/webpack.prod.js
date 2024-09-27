5456e6c39d6f6f64de0c5b44e8c2f6036cc88483
월하 <cerezo00@naver.com>
2024-09-27 14:24:23 +0900

    chore: webpack compression plugin 삭제

--- frontend/webpack.prod.js
+++ frontend/webpack.prod.js
@@ -1,6 +1,5 @@
 const { merge } = require('webpack-merge');
 const Dotenv = require('dotenv-webpack');
-const CompressionPlugin = require('compression-webpack-plugin');
 const common = require('./webpack.common.js');
 
 module.exports = () => {
@@ -13,10 +12,6 @@ module.exports = () => {
         systemvars: true,
         safe: true,
         ignoreStub: true,
-      }),
-      new CompressionPlugin({
-        algorithm: 'brotliCompress',
-        exclude: /\.(png|jpg|gif|webp|webm|map|ico)$/i,
       }),
     ],
     optimization: {
