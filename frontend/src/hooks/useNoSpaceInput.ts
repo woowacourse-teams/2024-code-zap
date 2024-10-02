0e652058d5eab1e8e1c5261c09d57292de6c5827
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-10-02 14:01:37 +0900

    refactor(hooks): useNoSpaceInput 에서 공백처럼 보이는 유니코드 막도록 추가

--- frontend/src/hooks/useNoSpaceInput.ts
+++ frontend/src/hooks/useNoSpaceInput.ts
@@ -4,7 +4,7 @@ export const useNoSpaceInput = (initValue: string = '') => {
   const [value, setValue] = useState(initValue);
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const noSpaceValue = e.target.value.replace(/[\s\u3164\uFEFF\u200B]+/gu, '');
+    const noSpaceValue = e.target.value.replace(/[\s\u3000\u115F\u1160\u2800\u3164\uFFA0\u200B\uFEFF]+/gu, '');
 
     setValue(noSpaceValue);
   };
