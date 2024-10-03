cb40c2ce1d46a956f150bfeda1d442742e56946f
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-03 12:34:52 +0900

    refactory(validation): 문자열 목록에 대해 유효성을 검증하는 객체 구현

---
+++ backend/src/main/java/codezap/global/validation/GroupedByteLengthValidator.java
@@ -0,0 +1,30 @@
+package codezap.global.validation;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class GroupedByteLengthValidator implements ConstraintValidator<ByteLength, List<String>> {
+
+    private int max;
+    private int min;
+
+    @Override
+    public void initialize(ByteLength constraintAnnotation) {
+        max = constraintAnnotation.max();
+        min = constraintAnnotation.min();
+    }
+
+    @Override
+    public boolean isValid(List<String> strings, ConstraintValidatorContext constraintValidatorContext) {
+        return strings.stream()
+                .allMatch(this::isValid);
+    }
+
+    public boolean isValid(String target) {
+        int byteLength = target.getBytes(StandardCharsets.UTF_8).length;
+        return min <= byteLength && byteLength <= max;
+    }
+}
