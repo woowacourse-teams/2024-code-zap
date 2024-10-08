f0983ab0a9cc33f98fa4014c0a4086608fea979d
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-10-08 20:27:42 +0900

    feat(hooks): useCustomNavigate JSDoc 추가

--- frontend/src/hooks/useCustomNavigate.ts
+++ frontend/src/hooks/useCustomNavigate.ts
@@ -1,5 +1,12 @@
 import { useNavigate, useLocation, NavigateOptions, To } from 'react-router-dom';
-
+/**
+ * useCustomNavigate - 현재 위치와 대상 위치를 비교하여 불필요한 네비게이션을 방지합니다.
+ *
+ * @returns {Function} 커스텀 네비게이트 함수
+ *   @param {To | number} to - 네비게이션 대상. 문자열 경로, 위치 객체 또는 히스토리 스택의 상대적 위치를 나타내는 숫자일 수 있습니다.
+ *   @param {NavigateOptions} [options] - 네비게이션 옵션 (선택적)
+ *
+ */
 export const useCustomNavigate = () => {
   const navigate = useNavigate();
   const location = useLocation();
