225430c17c38e3f60b86d3b361eac15030a07966
zangsu <zangsu_@naver.com>
2024-10-09 17:20:32 +0900

    refactor: RFC2616 에 따라 쿠키에 들어갈 수 있는 ASCII 값으로 변형을 위해 URLEncoder, URLDecoder 사용

--- backend/src/test/java/codezap/auth/provider/PlainCredentialProvider.java
+++ backend/src/test/java/codezap/auth/provider/PlainCredentialProvider.java
@@ -1,28 +1,36 @@
 package codezap.auth.provider;
 
+import java.net.URLDecoder;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import codezap.member.domain.Member;
 
 public class PlainCredentialProvider implements CredentialProvider {
 
-    private static final String DELIMITER = ";";
+    private static final String DELIMITER = URLEncoder.encode(";", StandardCharsets.UTF_8);
 
     @Override
     public String createCredential(Member member) {
-        return String.join(DELIMITER,
-                Long.toString(member.getId()),
-                member.getName(),
-                member.getPassword(),
-                member.getSalt());
+        return Stream.of(Long.toString(member.getId()), member.getName(), member.getPassword(), member.getSalt())
+                .map(value -> URLEncoder.encode(value, StandardCharsets.UTF_8))
+                .collect(Collectors.joining(DELIMITER));
     }
 
     @Override
     public Member extractMember(String credential) {
-        String[] memberInfo = credential.split(DELIMITER);
+        List<String> memberInfo = Arrays.stream(credential.split(DELIMITER))
+                .map(value -> URLDecoder.decode(value, StandardCharsets.UTF_8))
+                .toList();
         return new Member(
-                Long.parseLong(memberInfo[0]),
-                memberInfo[1],
-                memberInfo[2],
-                memberInfo[3]
+                Long.parseLong(memberInfo.get(0)),
+                memberInfo.get(1),
+                memberInfo.get(2),
+                memberInfo.get(3)
         );
     }
 }
