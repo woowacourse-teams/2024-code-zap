2624021b408c2e73065e80e7db7b1d18b0b30b2f
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-10-12 03:06:37 +0900

    feat(components): Toggle 컴포넌트

---
+++ frontend/src/components/Toggle/Toggle.style.ts
@@ -0,0 +1,55 @@
+import styled from '@emotion/styled';
+
+import { theme } from '@/style/theme';
+
+export const ToggleContainer = styled.div`
+  cursor: pointer;
+
+  position: relative;
+
+  overflow: hidden;
+  display: flex;
+
+  width: 12rem;
+  height: 2.375rem; /* Button medium size와 동일 */
+
+  background-color: ${theme.color.light.secondary_100};
+  border-radius: 20px;
+`;
+
+export const ToggleOption = styled.div<{ selected: boolean }>`
+  z-index: 1;
+
+  display: flex;
+  flex: 1;
+  gap: 1px;
+  align-items: center;
+  justify-content: center;
+
+  color: ${({ selected }) => (selected ? theme.color.light.white : theme.color.light.secondary_500)};
+
+  transition: color 0.3s ease;
+`;
+
+export const ToggleSlider = styled.div<{
+  isRight: boolean;
+  optionSliderColor: [string | undefined, string | undefined];
+}>`
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  transform: ${({ isRight }) => (isRight ? 'translateX(calc(100% - 4px))' : 'translateX(0)')};
+
+  width: calc(50%);
+  height: calc(100% - 4px);
+
+  background-color: ${({ isRight, optionSliderColor: [leftColor, rightColor] }) =>
+    isRight
+      ? (rightColor ?? theme.color.light.triadic_secondary_800)
+      : (leftColor ?? theme.color.light.analogous_secondary_800)};
+  border-radius: 18px;
+
+  transition:
+    transform 0.3s ease,
+    background-color 0.3s ease;
+`;
---
+++ frontend/src/components/Toggle/Toggle.tsx
@@ -0,0 +1,46 @@
+import { ReactNode } from 'react';
+
+import * as S from './Toggle.style';
+
+type ToggleOption<T extends string> = T;
+
+export interface ToggleProps<T extends string> {
+  options: [ToggleOption<T>, ToggleOption<T>];
+  optionSliderColor?: [string | undefined, string | undefined];
+  optionAdornments?: [ReactNode, ReactNode];
+  selectedOption: ToggleOption<T>;
+  switchOption: (option: ToggleOption<T>) => void;
+}
+
+const Toggle = <T extends string>({
+  options,
+  optionAdornments = [undefined, undefined],
+  optionSliderColor = [undefined, undefined],
+  selectedOption,
+  switchOption,
+}: ToggleProps<T>) => {
+  const [leftOption, rightOption] = options;
+  const [leftOptionAdornment, rightOptionAdornment] = optionAdornments;
+
+  const handleToggle = () => {
+    const newOption = selectedOption === leftOption ? rightOption : leftOption;
+
+    switchOption(newOption);
+  };
+
+  return (
+    <S.ToggleContainer onClick={handleToggle}>
+      <S.ToggleSlider isRight={selectedOption === rightOption} optionSliderColor={optionSliderColor} />
+      <S.ToggleOption selected={selectedOption === leftOption}>
+        {leftOptionAdornment ?? ''}
+        {leftOption}
+      </S.ToggleOption>
+      <S.ToggleOption selected={selectedOption === rightOption}>
+        {rightOption}
+        {rightOptionAdornment ?? ''}
+      </S.ToggleOption>
+    </S.ToggleContainer>
+  );
+};
+
+export default Toggle;
