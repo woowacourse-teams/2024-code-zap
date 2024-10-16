d2e20e52d5f45fc6dfdb69ab32c29c9e70626d8e
jayming66 <mjw8941@gmail.com>
2024-10-16 16:51:56 +0900

    refactor(Error): ApiError - switch-case 문을 매핑으로 변경

--- frontend/src/api/Error/ApiError.ts
+++ frontend/src/api/Error/ApiError.ts
@@ -5,6 +5,15 @@ import { HTTP_STATUS } from './statusCode';
  * errorCode: 예측된 에러의 식별자
  * detail: backend에서 보내는 에러 메시지
  */
+
+const errorNameMap: Record<number, string> = {
+  [HTTP_STATUS.BAD_REQUEST]: 'APIBadRequestError',
+  [HTTP_STATUS.UNAUTHORIZED]: 'APIAuthenticationError',
+  [HTTP_STATUS.FORBIDDEN]: 'APIForbiddenError',
+  [HTTP_STATUS.NOT_FOUND]: 'APINotFoundError',
+  [HTTP_STATUS.INTERNAL_SERVER_ERROR]: 'APIInternalServerError',
+};
+
 export class ApiError extends Error {
   statusCode: number;
   errorCode: string;
@@ -16,30 +25,6 @@ export class ApiError extends Error {
     this.statusCode = statusCode;
     this.errorCode = errorCode;
     this.detail = detail;
-
-    switch (statusCode) {
-      case HTTP_STATUS.BAD_REQUEST:
-        this.name = 'APIBadRequestError';
-        break;
-
-      case HTTP_STATUS.UNAUTHORIZED:
-        this.name = 'APIAuthenticationError';
-        break;
-
-      case HTTP_STATUS.FORBIDDEN:
-        this.name = 'APIForbiddenError';
-        break;
-
-      case HTTP_STATUS.NOT_FOUND:
-        this.name = 'APINotFoundError';
-        break;
-
-      case HTTP_STATUS.INTERNAL_SERVER_ERROR:
-        this.name = 'APIInternalServerError';
-        break;
-
-      default:
-        this.name = 'APIError';
-    }
+    this.name = errorNameMap[statusCode] || 'APIError';
   }
 }
