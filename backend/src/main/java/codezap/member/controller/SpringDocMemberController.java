fc524369a11f54e06bce3124bbf64d86c897ef63
zangsu <zangsu_@naver.com>
2024-10-18 13:29:49 +0900

    docs: 문서 수정

--- backend/src/main/java/codezap/member/controller/SpringDocMemberController.java
+++ backend/src/main/java/codezap/member/controller/SpringDocMemberController.java
@@ -40,11 +40,8 @@ public interface SpringDocMemberController {
     void checkUniquename(@RequestParam String name);
 
     @SecurityRequirement(name = "쿠키 인증 토큰")
-    @Operation(summary = "회원 정보 조회", description = "회원의 정보(아이디일, 닉네임)을 조회합니다.")
+    @Operation(summary = "회원 정보 조회", description = "회원의 정보(아이디)를 조회합니다.")
     @ApiResponse(responseCode = "200", description = "회원 정보 조회 성공")
-    @ApiErrorResponse(status = HttpStatus.FORBIDDEN, instance = "/members/1", errorCases = {
-            @ErrorCase(description = "로그인된 회원이 조회하려는 회원이 아닌 경우", exampleMessage = "본인의 정보만 조회할 수 있습니다.")
-    })
     @ApiErrorResponse(status = HttpStatus.NOT_FOUND, instance = "/members/1", errorCases = {
             @ErrorCase(description = "조회하려는 id 값인 회원이 없는 경우", exampleMessage = "식별자 1에 해당하는 회원이 존재하지 않습니다.")
     })
