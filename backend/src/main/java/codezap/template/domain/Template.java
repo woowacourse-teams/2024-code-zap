1cb184f159a0eca5a09e69a9ac6712dd2b6f6c08
kyum-q <109158497+kyum-q@users.noreply.github.com>
2024-10-22 13:47:38 +0900

    refactor(template): 기본 값 설정

--- backend/src/main/java/codezap/template/domain/Template.java
+++ backend/src/main/java/codezap/template/domain/Template.java
@@ -14,6 +14,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 
+import org.hibernate.annotations.ColumnDefault;
 import org.hibernate.annotations.Formula;
 
 import codezap.category.domain.Category;
@@ -57,6 +58,7 @@ public class Template extends BaseTimeEntity {
     private Long likesCount;
 
     @Column(nullable = false)
+    @ColumnDefault("'PUBLIC'")
     @Enumerated(EnumType.STRING)
     private Visibility visibility;
 
