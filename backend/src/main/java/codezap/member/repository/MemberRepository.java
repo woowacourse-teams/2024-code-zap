89db873d8b0aeac34a63a8a720469cd10f3d4be3
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-23 15:24:07 +0900

    feat(repository): MemberRepository JPQL 쿼리를 QueryDSL로 변경

--- backend/src/main/java/codezap/member/repository/MemberRepository.java
+++ backend/src/main/java/codezap/member/repository/MemberRepository.java
@@ -1,16 +1,39 @@
 package codezap.member.repository;
 
+import org.springframework.stereotype.Repository;
+
+import codezap.global.exception.CodeZapException;
+import codezap.global.exception.ErrorCode;
 import codezap.member.domain.Member;
+import lombok.AllArgsConstructor;
 
-public interface MemberRepository {
+@Repository
+@AllArgsConstructor
+public class MemberRepository {
 
-    Member fetchById(Long id);
+    private final MemberJpaRepository memberJpaRepository;
+    private final MemberQueryDSLRepository memberQueryDSLRepository;
 
-    Member fetchByName(String name);
+    public Member fetchById(Long id) {
+        return memberJpaRepository.findById(id).orElseThrow(
+                () -> new CodeZapException(ErrorCode.RESOURCE_NOT_FOUND, "식별자 " + id + "에 해당하는 멤버가 존재하지 않습니다."));
+    }
 
-    Member fetchByTemplateId(Long templateId);
+    public Member fetchByName(String name) {
+        return memberJpaRepository.findByName(name)
+                .orElseThrow(() -> new CodeZapException(ErrorCode.UNAUTHORIZED_ID, "존재하지 않는 아이디 " + name + " 입니다."));
+    }
 
-    boolean existsByName(String name);
+    public Member fetchByTemplateId(Long templateId) {
+        return memberQueryDSLRepository.findByTemplateId(templateId)
+                .orElseThrow(() -> new CodeZapException(ErrorCode.RESOURCE_NOT_FOUND, "템플릿에 대한 멤버가 존재하지 않습니다."));
+    }
 
-    Member save(Member member);
+    public boolean existsByName(String name) {
+        return memberJpaRepository.existsByName(name);
+    }
+
+    public Member save(Member member) {
+        return memberJpaRepository.save(member);
+    }
 }
