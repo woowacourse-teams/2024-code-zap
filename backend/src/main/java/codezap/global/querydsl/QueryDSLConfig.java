418dcb5195fe7d69df8b7e3f607c5f08c518ed60
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-23 15:27:57 +0900

    feat(querydsl): JPAQueryFactory 빈 등록을 위한 QueryDSL 설정 파일 작성

---
+++ backend/src/main/java/codezap/global/querydsl/QueryDSLConfig.java
@@ -0,0 +1,24 @@
+package codezap.global.querydsl;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import lombok.RequiredArgsConstructor;
+
+@Configuration
+@RequiredArgsConstructor
+public class QueryDSLConfig {
+
+    @PersistenceContext
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
