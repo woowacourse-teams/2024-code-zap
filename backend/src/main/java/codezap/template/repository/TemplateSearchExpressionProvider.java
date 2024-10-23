053cb2e70938c437f2c1c68360e20e2dc1fc9b47
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-24 01:19:35 +0900

    refactor(strategy): 키워드 검색 조건 전략 패턴 적

--- backend/src/main/java/codezap/template/repository/TemplateSearchExpressionProvider.java
+++ backend/src/main/java/codezap/template/repository/TemplateSearchExpressionProvider.java
@@ -10,22 +10,21 @@ import java.util.Optional;
 import org.springframework.stereotype.Component;
 
 import com.querydsl.core.types.dsl.BooleanExpression;
-import com.querydsl.core.types.dsl.Expressions;
-import com.querydsl.core.types.dsl.NumberExpression;
 import com.querydsl.jpa.JPAExpressions;
 
 import codezap.template.domain.Visibility;
+import codezap.template.repository.strategy.FullTextSearchSearchStrategy;
+import codezap.template.repository.strategy.LikeSearchStrategy;
+import lombok.RequiredArgsConstructor;
 
 @Component
+@RequiredArgsConstructor
 public class TemplateSearchExpressionProvider {
 
-    private static final String MATCH_FUNCTION = "function('fulltext_match', {0}, {1}, {2})";
+    private final LikeSearchStrategy likeSearchStrategy;
+    private final FullTextSearchSearchStrategy fullTextSearchStrategy;
+
     private static final int MINIMUM_KEYWORD_LENGTH = 3;
-    private static final int FULL_TEXT_FIELD_FIRST_ORDER = 0;
-    private static final int FULL_TEXT_FIELD_SECOND_ORDER = 1;
-    private static final int FULL_TEXT_KEYWORD_ORDER = 2;
-    private static final int NO_MATCHED_SCORE = 0;
-    private static final String WILD_CARD = "%";
 
     public BooleanExpression filterMember(Long memberId) {
         return Optional.ofNullable(memberId)
@@ -68,44 +67,9 @@ public class TemplateSearchExpressionProvider {
 
     private BooleanExpression createKeywordMatchExpression(String trimmedKeyword) {
         if (trimmedKeyword.length() < MINIMUM_KEYWORD_LENGTH) {
-            return matchedKeywordUsingLike(trimmedKeyword);
+            return likeSearchStrategy.matchedKeyword(trimmedKeyword);
         }
-        return matchedKeywordUsingFullTextIndex(trimmedKeyword);
-    }
 
-    public BooleanExpression matchedKeywordUsingLike(String trimmedKeyword) {
-        String likeSearchedKeyword = WILD_CARD + trimmedKeyword + WILD_CARD;
-        BooleanExpression titleMatch = template.title.likeIgnoreCase(likeSearchedKeyword);
-        BooleanExpression descriptionMatch = template.description.likeIgnoreCase(likeSearchedKeyword);
-        BooleanExpression sourceCodeMatch = template.id.in(
-                JPAExpressions
-                        .select(sourceCode.template.id)
-                        .from(sourceCode)
-                        .where(sourceCode.content.likeIgnoreCase(likeSearchedKeyword)
-                                .or(sourceCode.filename.likeIgnoreCase(likeSearchedKeyword)))
-        );
-        return titleMatch.or(descriptionMatch).or(sourceCodeMatch);
-    }
-
-
-    public BooleanExpression matchedKeywordUsingFullTextIndex(String trimmedKeyword) {
-        NumberExpression<Double> titleScore = getMatchedAccuracy(template.title, template.description, trimmedKeyword);
-        NumberExpression<Double> sourceCodeScore = getMatchedAccuracy(sourceCode.filename, sourceCode.content, trimmedKeyword);
-        return titleScore.gt(NO_MATCHED_SCORE).or(
-                template.id.in(JPAExpressions
-                        .select(sourceCode.template.id)
-                        .from(sourceCode)
-                        .where(sourceCodeScore.gt(NO_MATCHED_SCORE))
-                )
-        );
-    }
-
-    public NumberExpression<Double> getMatchedAccuracy(Object... args) {
-        return Expressions.numberTemplate(Double.class,
-                MATCH_FUNCTION,
-                args[FULL_TEXT_FIELD_FIRST_ORDER],
-                args[FULL_TEXT_FIELD_SECOND_ORDER],
-                args[FULL_TEXT_KEYWORD_ORDER]
-        );
+        return fullTextSearchStrategy.matchedKeyword(trimmedKeyword);
     }
 }
