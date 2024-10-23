9695c6ee6578d4dae316eb26c7f92938632fc597
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-23 15:25:25 +0900

    feat(repository): TemplateTagRepository JPQL 쿼리를 QueryDSL로 변경

--- backend/src/main/java/codezap/tag/repository/TemplateTagRepository.java
+++ backend/src/main/java/codezap/tag/repository/TemplateTagRepository.java
@@ -2,27 +2,53 @@ package codezap.tag.repository;
 
 import java.util.List;
 
+import org.springframework.stereotype.Repository;
+
 import codezap.tag.domain.Tag;
 import codezap.template.domain.Template;
 import codezap.template.domain.TemplateTag;
+import lombok.AllArgsConstructor;
 
-public interface TemplateTagRepository {
+@Repository
+@AllArgsConstructor
+public class TemplateTagRepository {
 
-    List<TemplateTag> findAllByTemplate(Template template);
+    private final TemplateTagJpaRepository templateTagJpaRepository;
+    private final TemplateTagQueryDSLRepository templateTagQueryDSLRepository;
 
-    List<Tag> findAllTagsByTemplate(Template template);
+    public List<TemplateTag> findAllByTemplate(Template template) {
+        return templateTagQueryDSLRepository.findAllByTemplateId(template.getId());
+    }
 
-    List<Tag> findAllTagDistinctByMemberId(Long memberId);
+    public List<Tag> findAllTagsByTemplate(Template template) {
+        return templateTagQueryDSLRepository.findAllTagsByTemplate(template);
+    }
 
-    List<TemplateTag> findAllByTemplateId(Long templateId);
+    public List<Tag> findAllTagDistinctByMemberId(Long memberId) {
+        return templateTagQueryDSLRepository.findAllTagDistinctByMemberId(memberId);
+    }
 
-    List<TemplateTag> findAllByTemplateIdsIn(List<Long> templateIds);
+    public List<TemplateTag> findAllByTemplateId(Long templateId) {
+        return templateTagQueryDSLRepository.findAllByTemplateId(templateId);
+    }
 
-    TemplateTag save(TemplateTag templateTag);
+    public List<TemplateTag> findAllByTemplateIdsIn(List<Long> templateIds) {
+        return templateTagQueryDSLRepository.findAllByTemplateIdsIn(templateIds);
+    }
 
-    <S extends TemplateTag> List<S> saveAll(Iterable<S> entities);
+    public TemplateTag save(TemplateTag templateTag) {
+        return templateTagJpaRepository.save(templateTag);
+    }
 
-    void deleteAllByTemplateId(Long templateId);
+    public List<TemplateTag> saveAll(Iterable<TemplateTag> entities) {
+        return templateTagJpaRepository.saveAll(entities);
+    }
 
-    void deleteAllByTemplateIds(List<Long> templateIds);
+    public void deleteAllByTemplateId(Long templateId) {
+        templateTagJpaRepository.deleteAllByTemplateId(templateId);
+    }
+
+    public void deleteAllByTemplateIds(List<Long> templateIds) {
+        templateTagQueryDSLRepository.deleteAllByTemplateIds(templateIds);
+    }
 }
