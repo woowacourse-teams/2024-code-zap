c28860e24f06528cbe0791345cf47c18ae2d383a
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-10-23 17:01:49 +0900

    feat(HotTopicCarousel): 인기 토픽 캐로셀에 설명 추가

--- frontend/src/pages/TemplateExplorePage/components/HotTopicCarousel/HotTopicCarousel.style.ts
+++ frontend/src/pages/TemplateExplorePage/components/HotTopicCarousel/HotTopicCarousel.style.ts
@@ -1,16 +1,67 @@
 import styled from '@emotion/styled';
 
+import { theme } from '@/style/theme';
+
 export const Topic = styled.button<{ background: string; border: string; isSelected: boolean }>`
-  cursor: pointer; /* 커서 스타일 추가 */
+  cursor: pointer;
+
+  position: relative;
+
+  display: flex;
+  align-items: flex-end;
 
   width: 100%;
   height: 100%;
+  padding: 1rem;
 
-  background-color: ${({ background }) => background};
-  border: ${({ isSelected, border }) => (isSelected ? `2px solid ${border}` : 'none')};
+  background-image: url(${({ background }) => background});
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: cover;
   border-radius: 12px;
+  box-shadow: ${({ isSelected, border }) => (isSelected ? `0 0 0 2px ${border}` : 'none')};
 
-  &:hover {
-    border: 2px solid ${({ border }) => border};
+  transition: box-shadow 0.2s ease-in-out;
+
+  &::after {
+    pointer-events: none;
+    content: '';
+
+    position: absolute;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+
+    background-color: rgba(0, 0, 0, 0);
+    border-radius: 12px;
+
+    transition: background-color 0.3s;
   }
+
+  &:hover::after {
+    background-color: rgba(0, 0, 0, 0.2);
+  }
+`;
+
+export const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  justify-content: flex-start;
+`;
+
+export const Title = styled.div`
+  width: fit-content;
+  padding: 0.25rem 0.5rem;
+  border: 1px solid ${theme.color.light.secondary_800};
+  border-radius: 8px;
+`;
+
+export const Description = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 0.25rem;
+  align-items: flex-start;
+  justify-content: flex-start;
 `;
--- frontend/src/pages/TemplateExplorePage/components/HotTopicCarousel/HotTopicCarousel.tsx
+++ frontend/src/pages/TemplateExplorePage/components/HotTopicCarousel/HotTopicCarousel.tsx
@@ -1,47 +1,54 @@
 import { Text } from '@/components';
-import { TAG_COLORS } from '@/style/tagColors';
+import { useWindowWidth } from '@/hooks';
+import { HOT_TOPIC } from '@/service/hotTopic';
+import { BREAKING_POINT } from '@/style/styleConstants';
 import { theme } from '@/style/theme';
 
 import { Carousel } from '../';
 import * as S from './HotTopicCarousel.style';
 
-const HOT_TOPIC = [
-  { key: 1, content: '우테코', tagIds: [359], color: TAG_COLORS[3] },
-  { key: 2, content: '프리코스', tagIds: [364], color: TAG_COLORS[4] },
-  { key: 4, content: '자바스크립트', tagIds: [41, 211, 329, 351, 249, 360], color: TAG_COLORS[0] },
-  { key: 3, content: '자바', tagIds: [73, 358, 197], color: TAG_COLORS[1] },
-  { key: 5, content: '코틀린', tagIds: [361, 363], color: TAG_COLORS[2] },
-  { key: 8, content: '스프링', tagIds: [14, 198], color: TAG_COLORS[7] },
-  { key: 7, content: '리액트', tagIds: [50, 289, 318], color: TAG_COLORS[6] },
-  { key: 8, content: '안드로이드', tagIds: [362], color: TAG_COLORS[8] },
-  { key: 9, content: '알고리즘', tagIds: [261, 316, 253, 144, 143], color: TAG_COLORS[5] },
-];
-
 interface Props {
-  selectTopic: ({ tagIds, content }: { tagIds: number[]; content: string }) => void;
+  selectTopic: ({ tagIds, topic }: { tagIds: number[]; topic: string }) => void;
   selectedHotTopic: string;
 }
 
-const HotTopicCarousel = ({ selectTopic, selectedHotTopic }: Props) => (
-  <Carousel
-    items={HOT_TOPIC.map(({ key, content, tagIds, color }) => ({
-      id: key,
-      content: (
-        <S.Topic
-          isSelected={selectedHotTopic === content}
-          background={color.background}
-          border={color.border}
-          onClick={() => {
-            selectTopic({ tagIds, content });
-          }}
-        >
-          <Text.Large color={theme.color.light.secondary_800} weight='bold'>
-            {content}
-          </Text.Large>
-        </S.Topic>
-      ),
-    }))}
-  />
-);
+const HotTopicCarousel = ({ selectTopic, selectedHotTopic }: Props) => {
+  const windowWidth = useWindowWidth();
+  const isMobile = windowWidth <= BREAKING_POINT.MOBILE;
+
+  return (
+    <Carousel
+      items={HOT_TOPIC.map(({ topic, description, subDescription, tagIds, color, bg }) => ({
+        id: topic,
+        content: (
+          <S.Topic
+            isSelected={selectedHotTopic === topic}
+            background={bg}
+            border={color}
+            onClick={() => {
+              selectTopic({ tagIds, topic });
+            }}
+          >
+            <S.Content>
+              <S.Title>
+                <Text.Large color={theme.color.light.secondary_800} weight='bold'>
+                  {topic}
+                </Text.Large>
+              </S.Title>
+              {!isMobile && (
+                <S.Description>
+                  <Text.Small color={theme.color.light.secondary_800} weight='bold'>
+                    {description}
+                  </Text.Small>
+                  <Text.Small color={theme.color.light.secondary_800}>{subDescription}</Text.Small>
+                </S.Description>
+              )}
+            </S.Content>
+          </S.Topic>
+        ),
+      }))}
+    />
+  );
+};
 
 export default HotTopicCarousel;
