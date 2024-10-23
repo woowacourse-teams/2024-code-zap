d2505dbdb3a254abe09bb8cf881fdadcebe5b39a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-23 15:26:12 +0900

    feat(repository): SourceCodeRepository JPQL 쿼리를 QueryDSL로 변경

--- backend/src/main/java/codezap/template/repository/SourceCodeJpaRepository.java
+++ backend/src/main/java/codezap/template/repository/SourceCodeJpaRepository.java
@@ -4,39 +4,18 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Modifying;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
-import codezap.global.exception.CodeZapException;
-import codezap.global.exception.ErrorCode;
 import codezap.template.domain.SourceCode;
 import codezap.template.domain.Template;
 
 @SuppressWarnings("unused")
-public interface SourceCodeJpaRepository extends SourceCodeRepository, JpaRepository<SourceCode, Long> {
-
-    default SourceCode fetchById(Long id) {
-        return findById(id).orElseThrow(
-                () -> new CodeZapException(ErrorCode.RESOURCE_NOT_FOUND, "식별자 " + id + "에 해당하는 소스 코드가 존재하지 않습니다."));
-    }
+public interface SourceCodeJpaRepository extends JpaRepository<SourceCode, Long> {
 
     List<SourceCode> findAllByTemplate(Template template);
-
-    default SourceCode fetchByTemplateAndOrdinal(Template template, int ordinal) {
-        return findByTemplateAndOrdinal(template, ordinal)
-                .orElseThrow(
-                        () -> new CodeZapException(ErrorCode.RESOURCE_NOT_FOUND,
-                                "템플릿에 " + ordinal + "번째 소스 코드가 존재하지 않습니다."));
-    }
 
     Optional<SourceCode> findByTemplateAndOrdinal(Template template, int ordinal);
 
     List<SourceCode> findAllByTemplateAndOrdinal(Template template, int ordinal);
 
     int countByTemplate(Template template);
-
-    @Modifying(clearAutomatically = true)
-    @Query("DELETE FROM SourceCode s WHERE s.template.id in :templateIds")
-    void deleteAllByTemplateIds(@Param("templateIds") List<Long> templateIds);
 }
