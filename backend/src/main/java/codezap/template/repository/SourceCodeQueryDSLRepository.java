e4f7d5e51cf46dcf5295d24fe38a0dca51537b9d
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-23 15:26:12 +0900

    feat(repository): SourceCodeRepository JPQL 쿼리를 QueryDSL로 변경

---
+++ backend/src/main/java/codezap/template/repository/SourceCodeQueryDSLRepository.java
@@ -0,0 +1,25 @@
+package codezap.template.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.stereotype.Repository;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+
+import codezap.template.domain.QSourceCode;
+import lombok.AllArgsConstructor;
+
+@Repository
+@AllArgsConstructor
+public class SourceCodeQueryDSLRepository {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Modifying(clearAutomatically = true)
+    public void deleteAllByTemplateIds(List<Long> templateIds) {
+        queryFactory.delete(QSourceCode.sourceCode)
+                .where(QSourceCode.sourceCode.template.id.in(templateIds))
+                .execute();
+    }
+}
