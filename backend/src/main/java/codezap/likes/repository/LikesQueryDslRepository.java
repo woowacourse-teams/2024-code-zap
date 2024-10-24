2e39e4024392a2a218c748a6879429637442aa11
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-10-24 14:37:41 +0900

    fix(repository): like한 템플릿 조회 메서드 충돌 해결

--- backend/src/main/java/codezap/likes/repository/LikesQueryDslRepository.java
+++ backend/src/main/java/codezap/likes/repository/LikesQueryDslRepository.java
@@ -2,12 +2,21 @@ package codezap.likes.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 
+import codezap.likes.domain.Likes;
 import codezap.likes.domain.QLikes;
+import codezap.template.domain.QTemplate;
+import codezap.template.domain.Template;
+import codezap.template.domain.Visibility;
 import lombok.AllArgsConstructor;
 
 @Repository
