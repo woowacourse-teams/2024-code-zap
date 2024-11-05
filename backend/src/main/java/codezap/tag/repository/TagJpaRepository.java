4a09a4ecb1c96f091857c09f428938efb5ede8c8
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-11-05 22:14:19 +0900

    refactor(repository): 사용되지 않는 메서드 제

--- backend/src/main/java/codezap/tag/repository/TagJpaRepository.java
+++ backend/src/main/java/codezap/tag/repository/TagJpaRepository.java
@@ -14,11 +14,6 @@ import codezap.tag.domain.Tag;
 @SuppressWarnings("unused")
 public interface TagJpaRepository extends JpaRepository<Tag, Long> {
 
-    default Tag fetchById(Long id) {
-        return findById(id).orElseThrow(
-                () -> new CodeZapException(ErrorCode.RESOURCE_NOT_FOUND, "식별자 " + id + "에 해당하는 태그가 존재하지 않습니다."));
-    }
-
     @Query(value = "SELECT * FROM tag WHERE tag.name = BINARY :name", nativeQuery = true)
     Optional<Tag> findByName(@Param("name") String name);
 
