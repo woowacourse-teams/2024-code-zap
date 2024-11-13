d8d38736438dbcb6616d8620533a3b17f1400151
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-11-13 22:24:05 +0900

    refactor(template): 페이지네이션 페이지 수 응답 변수 이름 변

--- backend/src/main/java/codezap/template/dto/response/FindAllTemplatesResponse.java
+++ backend/src/main/java/codezap/template/dto/response/FindAllTemplatesResponse.java
@@ -6,7 +6,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 
 public record FindAllTemplatesResponse(
         @Schema(description = "최대 페이지 개수, 최대 5개입니다.", example = "1")
-        int maxPages,
+        int paginationSizes,
         @Schema(description = "템플릿 목록")
         List<FindAllTemplateItemResponse> templates
 ) {
