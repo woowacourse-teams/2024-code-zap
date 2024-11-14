73b4e0f897c229d5e873666d4f8119cd517fcdad
정회성 <ghltjd3@gmail.com>
2024-11-14 11:37:08 +0900

    백엔드 로그 리팩토링 (#859)

--- backend/src/test/java/codezap/template/domain/TemplateTest.java
+++ backend/src/test/java/codezap/template/domain/TemplateTest.java
@@ -1,7 +1,6 @@
 package codezap.template.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
