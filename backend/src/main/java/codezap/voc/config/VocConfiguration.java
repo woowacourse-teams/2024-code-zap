0f15c67d14f04748f73df9e1651ce81889228209
재우 <Akaps6768@gmail.com>
2024-11-14 10:27:50 +0900

    feat(voc): http client 설정

--- backend/src/main/java/codezap/voc/config/VocConfiguration.java
+++ backend/src/main/java/codezap/voc/config/VocConfiguration.java
@@ -1,4 +1,9 @@
 package codezap.voc.config;
 
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan
 public class VocConfiguration {
 }
--- backend/src/main/java/codezap/voc/config/VocProperties.java
+++ backend/src/main/java/codezap/voc/config/VocProperties.java
@@ -1,4 +1,17 @@
 package codezap.voc.config;
 
+import java.time.Duration;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@ConfigurationProperties("voc.http.client")
+@RequiredArgsConstructor
+@Getter
 public class VocProperties {
+    private final String baseUrl;
+    private final Duration connectTimeout;
+    private final Duration readTimeout;
 }
