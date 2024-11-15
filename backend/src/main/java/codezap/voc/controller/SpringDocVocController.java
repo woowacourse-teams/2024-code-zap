b543c9aab957de83df8ac4cc492fb04de7d2c436
재우 <Akaps6768@gmail.com>
2024-11-15 11:15:18 +0900

    docs(voc): 문의하기 api 명세 수정

--- backend/src/main/java/codezap/voc/controller/SpringDocVocController.java
+++ backend/src/main/java/codezap/voc/controller/SpringDocVocController.java
@@ -4,11 +4,12 @@ import org.springframework.http.HttpStatus;
 
 import codezap.global.swagger.error.ApiErrorResponse;
 import codezap.global.swagger.error.ErrorCase;
-import codezap.member.domain.Member;
 import codezap.voc.dto.VocRequest;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
+@Tag(name = "문의하기")
 public interface SpringDocVocController {
 
     @Operation(summary = "문의하기")
@@ -22,5 +23,5 @@ public interface SpringDocVocController {
                     @ErrorCase(description = "문의 내용이 10,000 글자를 초과한 경우.", exampleMessage = "문의 내용은 최대 10,000 글자까지 입력할 수 있습니다.")
             }
     )
-    void contact(Member member, VocRequest request);
+    void contact(VocRequest request);
 }
--- backend/src/main/java/codezap/voc/dto/VocRequest.java
+++ backend/src/main/java/codezap/voc/dto/VocRequest.java
@@ -1,6 +1,8 @@
 package codezap.voc.dto;
 
+import jakarta.annotation.Nullable;
 import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
@@ -11,10 +13,23 @@ public record VocRequest(
         @Schema(description = "문의 내용", example = "코드잽 정말 잘 사용하고 있어요.")
         @NotEmpty(message = "문의 내용은 비어있을 수 없습니다.")
         @Size(min = 20, max = 10_000, message = "문의 내용은 최소 20자, 최대 10,000 자 입력할 수 있습니다.")
-        String content,
+        String message,
 
         @Schema(description = "이메일(선택)", example = "codezap2024@gmail.com")
         @Email(regexp = "[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "올바른 형식의 이메일 주소여야 합니다.") // Regex from RFC 5322
-        String email
+        String email,
+
+        @Nullable
+        @Schema(description = "사용자 db id(선택)", example = "1")
+        @Min(1)
+        Long memberId,
+
+        @Nullable
+        @Schema(description = "사용자 아이디(선택)", example = "만두")
+        @Size(max = 255, message = "아이디는 255자 이하로 입력해주세요.")
+        String name
 ) {
+    public VocRequest(String message, String email) {
+        this(message, email, null, null);
+    }
 }
