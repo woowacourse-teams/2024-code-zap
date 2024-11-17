01d0337af7bb8b4d0604c4bc7d68f67b36e8ad47
재우 <Akaps6768@gmail.com>
2024-11-17 16:54:03 +0900

    feat(voc): add error handler and interceptor for voc rest client

--- backend/src/main/java/codezap/voc/config/VocConfiguration.java
+++ backend/src/main/java/codezap/voc/config/VocConfiguration.java
@@ -1,27 +1,41 @@
 package codezap.voc.config;
 
-import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.BufferingClientHttpRequestFactory;
+import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
-@ConfigurationPropertiesScan
+@EnableConfigurationProperties(VocProperties.class)
 @RequiredArgsConstructor
+@Slf4j
 public class VocConfiguration {
 
     private final VocProperties properties;
 
     @Bean
     public RestClient.Builder restClientBuilder() {
+        return RestClient.builder()
+                .baseUrl(properties.getBaseUrl())
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .defaultStatusHandler(new VocResponseErrorHandler())
+                .requestInterceptor(new VocClientHttpRequestInterceptor())
+                .requestFactory(requestFactory());
+    }
+
+    private ClientHttpRequestFactory requestFactory() {
         var httpRequestFactory = new SimpleClientHttpRequestFactory();
         httpRequestFactory.setConnectTimeout(properties.getConnectTimeout());
         httpRequestFactory.setReadTimeout(properties.getReadTimeout());
-        return RestClient.builder()
-                .baseUrl(properties.getBaseUrl())
-                .requestFactory(httpRequestFactory);
+        return new BufferingClientHttpRequestFactory(httpRequestFactory);
     }
 }
