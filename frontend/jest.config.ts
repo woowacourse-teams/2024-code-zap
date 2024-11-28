b323883f58506e237ed601adcba68f22dc170e4e
Hyelim Choi <HyelimChoi01@gmail.com>
2024-11-28 22:19:01 +0900

    chore: 경로 변경 반영

--- frontend/jest.config.ts
+++ frontend/jest.config.ts
@@ -12,7 +12,7 @@ const config: Config = {
   transform: {
     '^.+\\.tsx?$': 'ts-jest',
     '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
-      '<rootDir>/src/mocks/fileTransformer.js',
+      '<rootDir>/src/mocks/settings/fileTransformer.js',
   },
   clearMocks: true,
   moduleNameMapper: {
