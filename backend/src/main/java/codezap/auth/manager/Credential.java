e7c68c0ff5873caa866b9fe52a169431c4bed624
zangsu <zangsu_@naver.com>
2024-12-13 22:11:20 +0900

    refactor: 포맷 적용 및 상수화 적용

--- backend/src/main/java/codezap/auth/manager/Credential.java
+++ backend/src/main/java/codezap/auth/manager/Credential.java
@@ -8,10 +8,11 @@ public record Credential(String type, String value) {
     private static final String DELIMITER = " ";
     private static final int AUTHORIZATION_TYPE_INDEX = 0;
     private static final int CREDENTIAL_TYPE_INDEX = 1;
+    private static final int CREDENTIAL_LENGTH = 2;
 
     public static Credential from(String authorizationHeader) {
         String[] typeAndCredential = authorizationHeader.split(DELIMITER);
-        if(typeAndCredential.length != 2) {
+        if (typeAndCredential.length != CREDENTIAL_LENGTH) {
             throw new CodeZapException(ErrorCode.UNAUTHORIZED_USER, "인증 헤더의 값이 잘못되었습니다.");
         }
         return new Credential(typeAndCredential[AUTHORIZATION_TYPE_INDEX], typeAndCredential[CREDENTIAL_TYPE_INDEX]);
