4fc9b5a0bd71296c316c3e13dd81f6e463708032
zangsu <zangsu_@naver.com>
2024-12-15 22:45:16 +0900

    docs: Javadoc 추가

--- backend/src/main/java/codezap/auth/manager/CredentialManager.java
+++ backend/src/main/java/codezap/auth/manager/CredentialManager.java
@@ -4,6 +4,9 @@ import codezap.auth.dto.Credential;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+/**
+ * Credential 정보를 Http 응답에 설정하기 위한 클래스입니다.
+ */
 public interface CredentialManager {
 
     Credential getCredential(HttpServletRequest httpServletRequest);
--- backend/src/main/java/codezap/auth/provider/CredentialProvider.java
+++ backend/src/main/java/codezap/auth/provider/CredentialProvider.java
@@ -4,6 +4,9 @@ import codezap.auth.dto.LoginMember;
 import codezap.auth.dto.Credential;
 import codezap.member.domain.Member;
 
+/**
+ * 로그인 정보를 사용해 규격화된 Credential 객체를 생성하는 클래스입니다.
+ */
 public interface CredentialProvider {
 
     Credential createCredential(LoginMember loginMember);
