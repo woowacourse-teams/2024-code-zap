7e438853980512be9d1cc41cc48657d22253e80a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2024-12-19 12:48:20 +0900

    fix(domain): tag 유니크 제약 조건 추가

--- backend/src/main/java/codezap/tag/domain/Tag.java
+++ backend/src/main/java/codezap/tag/domain/Tag.java
@@ -27,7 +27,7 @@ public class Tag extends BaseTimeEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
 
     public Tag(String name) {
