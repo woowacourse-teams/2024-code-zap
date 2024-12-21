7e837ad51433cdddc0d0928c0b0a56f2c765e72c
Hyelim Choi <HyelimChoi01@gmail.com>
2024-12-22 00:25:20 +0900

    chore: 전역 스타일 디자인 시스템으로 이전

---
+++ design-system/style/styleConstants.ts
@@ -0,0 +1,12 @@
+export const ICON_SIZE = {
+  X_SMALL: 14,
+  SMALL: 16,
+  MEDIUM_SMALL: 18,
+  MEDIUM_LARGE: 20,
+  LARGE: 24,
+  X_LARGE: 32,
+} as const;
+
+export const BREAKING_POINT = {
+  MOBILE: 768,
+};
