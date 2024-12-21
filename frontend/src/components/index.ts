e0e524448d0ebe40bff53b639e4cfe8d195d361d
Hyelim Choi <HyelimChoi01@gmail.com>
2024-12-22 00:26:20 +0900

    chore: 버튼 컴포넌트 이전

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -1,4 +1,4 @@
-export { default as Button } from './Button/Button';
+export { default as Button } from '@design/Button/Button';
 export { default as Dropdown } from './Dropdown/Dropdown';
 export { default as Flex } from './Flex/Flex';
 export { default as Header } from './Header/Header';
