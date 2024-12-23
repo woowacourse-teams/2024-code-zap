a278e1646eff36c028b2a931dd8458ea3b51a355
zangsu <zangsu_@naver.com>
2024-12-23 22:23:30 +0900

    refactor: 테스트 포트를 RANDOM_PORT 로 변경

--- backend/src/test/java/codezap/global/IntegrationTest.java
+++ backend/src/test/java/codezap/global/IntegrationTest.java
@@ -6,11 +6,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@SpringBootTest
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 @EnableConfigurationProperties(CorsProperties.class)
 public class IntegrationTest {
 
