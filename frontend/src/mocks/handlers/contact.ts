a51f056eeaac1ce369b776214bcbb2df8d94935d
헤인 <157036488+Hain-tain@users.noreply.github.com>
2024-12-23 17:17:50 +0900

    refactor(handlers): contactHandlers 응답 201로 변경

--- frontend/src/mocks/handlers/contact.ts
+++ frontend/src/mocks/handlers/contact.ts
@@ -4,4 +4,4 @@ import { API_URL } from '@/api';
 import { END_POINTS } from '@/routes';
 import { mockResponse } from '@/utils/mockResponse';
 
-export const contactHandlers = [http.get(`${API_URL}${END_POINTS.CONTACT}`, () => mockResponse({ status: 200 }))];
+export const contactHandlers = [http.get(`${API_URL}${END_POINTS.CONTACT}`, () => mockResponse({ status: 201 }))];
