f96d2792c08d706b452b24f602738d2733f67762
HoeSeong123 <ghltjd3@gmail.com>
2024-12-27 13:41:30 +0900

    feat: 카테고리 생성 dto에 최소 순서 검증 추가

--- backend/src/main/java/codezap/category/controller/SpringDocCategoryController.java
+++ backend/src/main/java/codezap/category/controller/SpringDocCategoryController.java
@@ -31,6 +31,7 @@ public interface SpringDocCategoryController {
     @ApiErrorResponse(status = HttpStatus.BAD_REQUEST, instance = "/categories", errorCases = {
             @ErrorCase(description = "모든 필드 중 null인 값이 있는 경우", exampleMessage = "카테고리 이름이 null 입니다."),
             @ErrorCase(description = "카테고리 이름이 15자를 초과한 경우", exampleMessage = "카테고리 이름은 최대 15자까지 입력 가능합니다."),
+            @ErrorCase(description = "카테고리의 순서가 1보다 작은 경우", exampleMessage = "카테고리의 순서는 1 이상이어야 합니다."),
     })
     @ApiErrorResponse(status = HttpStatus.CONFLICT, instance = "/categories", errorCases = {
             @ErrorCase(description = "동일한 이름의 카테고리가 존재하는 경우", exampleMessage = "이름이 Spring 인 카테고리가 이미 존재합니다."),
--- backend/src/main/java/codezap/category/dto/request/CreateCategoryRequest.java
+++ backend/src/main/java/codezap/category/dto/request/CreateCategoryRequest.java
@@ -1,5 +1,6 @@
 package codezap.category.dto.request;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
@@ -16,6 +17,7 @@ public record CreateCategoryRequest(
 
         @Schema(description = "카테고리 순서", example = "1")
         @NotNull(message = "카테고리 순서가 null 입니다.", groups = NotNullGroup.class)
+        @Min(value = 1, message = "카테고리의 순서는 1 이상이어야 합니다.")
         Long ordinal
 ) {
 }
