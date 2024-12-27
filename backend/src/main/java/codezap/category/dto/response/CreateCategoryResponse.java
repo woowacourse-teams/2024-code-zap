26a0fabb417d219766cb63cabf4ef56f970841a5
HoeSeong123 <ghltjd3@gmail.com>
2024-12-27 14:18:07 +0900

    feat(response): 카테고리 생성 응답 api 변경

--- backend/src/main/java/codezap/category/dto/response/CreateCategoryResponse.java
+++ backend/src/main/java/codezap/category/dto/response/CreateCategoryResponse.java
@@ -7,9 +7,11 @@ public record CreateCategoryResponse(
         @Schema(description = "카테고리 식별자", example = "1")
         Long id,
         @Schema(description = "카테고리 이름", example = "Spring")
-        String name
+        String name,
+        @Schema(description = "카테고리 순서", example = "1")
+        Integer ordinal
 ) {
     public static CreateCategoryResponse from(Category category) {
-        return new CreateCategoryResponse(category.getId(), category.getName());
+        return new CreateCategoryResponse(category.getId(), category.getName(), category.getOrdinal());
     }
 }
