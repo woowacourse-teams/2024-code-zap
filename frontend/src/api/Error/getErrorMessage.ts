83d186befbc3c83fbcbb445c6e8884db3cc6ada7
jayming66 <mjw8941@gmail.com>
2025-01-01 10:29:31 +0900

    feat(Error): 에러메시지 기본값 변경

--- frontend/src/api/Error/getErrorMessage.ts
+++ frontend/src/api/Error/getErrorMessage.ts
@@ -2,8 +2,8 @@ import { END_POINTS } from '@/routes';
 
 import { ERROR_CODE } from './errorCode';
 
-// TODO: errorCode magic number
 export const getErrorMessage = (errorCode: number, instance: string) => {
+  // Authorization
   if (errorCode === ERROR_CODE.NOT_FOUND_RESOURCE && instance === END_POINTS.CHECK_NAME) {
     return '중복된 아이디입니다';
   }
@@ -16,5 +16,9 @@ export const getErrorMessage = (errorCode: number, instance: string) => {
     return '비밀번호를 확인해주세요.';
   }
 
-  return 'APIError 입니다';
+  // 이거 멤버 ID 어떻게 가져오지,, 파라미터가 필요할 땐 백엔드의 도움을 받아야하나
+  // if (errorCode === ERROR_CODE.NOT_FOUND_ENDPOINT && instance === END_POINTS.memberTemplates(memberId)) {
+  // }
+
+  return '';
 };
