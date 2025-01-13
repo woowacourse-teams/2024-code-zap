6e9a2051b465c77b3bdd594f5593f1d2308ceef3
Hyelim Choi <HyelimChoi01@gmail.com>
2025-01-14 00:03:04 +0900

    chore: 스토리북 컨텍스트 공유

--- design-system/.storybook/preview.tsx
+++ design-system/.storybook/preview.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 import { ThemeProvider } from '@emotion/react';
 import type { Preview } from '@storybook/react';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import GlobalStyles from '../style/GlobalStyles';
 import { theme } from '../style/theme';
+import { AuthProvider, ToastProvider } from '../../frontend/src/contexts';
+import { MemoryRouter } from 'react-router-dom';
+
+const queryClient = new QueryClient();
 
 const preview: Preview = {
   parameters: {
@@ -17,10 +21,18 @@ const preview: Preview = {
   },
   decorators: [
     (Story) => (
-      <ThemeProvider theme={theme}>
-        <GlobalStyles />
-        <Story />
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <ToastProvider>
+            <ThemeProvider theme={theme}>
+              <GlobalStyles />
+              <MemoryRouter>
+                <Story />
+              </MemoryRouter>
+            </ThemeProvider>
+          </ToastProvider>
+        </AuthProvider>
+      </QueryClientProvider>
     ),
   ],
 };
