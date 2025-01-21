22ddaba3fd3f6219761d533b5edbafcd19ec381a
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2025-01-21 21:52:15 +0900

    style: import 최적화

--- backend/src/main/java/codezap/template/dto/request/CreateTemplateRequest.java
+++ backend/src/main/java/codezap/template/dto/request/CreateTemplateRequest.java
@@ -8,10 +8,10 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import codezap.global.validation.ByteLength;
+import codezap.global.validation.ValidatedOrdinalRequest;
 import codezap.global.validation.ValidationGroups.NotNullGroup;
 import codezap.global.validation.ValidationGroups.SizeCheckGroup;
 import codezap.template.domain.Visibility;
-import codezap.global.validation.ValidatedOrdinalRequest;
 import codezap.template.dto.request.validation.ValidatedSourceCodesCountRequest;
 import io.swagger.v3.oas.annotations.media.Schema;
 
