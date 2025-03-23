f00027ab8f9e208f60aba741116bd7c595108654
Jeongwoo Park <wahoo9040@naver.com>
2025-03-23 16:12:47 +0900

    feat: add Header i18n

--- frontend/src/i18n/index.ts
+++ frontend/src/i18n/index.ts
@@ -4,7 +4,9 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+import headerEn from './en/Header.json';
 import landingEn from './en/LandingPage.json';
+import headerKo from './ko/Header.json';
 import landingKo from './ko/LandingPage.json';
 
 i18n
@@ -13,13 +15,15 @@ i18n
   .init({
     fallbackLng: 'ko',
     lng: 'ko',
-    ns: ['LandingPage'],
+    ns: ['LandingPage', 'Header'],
     resources: {
       ko: {
         LandingPage: landingKo,
+        Header: headerKo,
       },
       en: {
         LandingPage: landingEn,
+        Header: headerEn,
       },
     },
     interpolation: {
