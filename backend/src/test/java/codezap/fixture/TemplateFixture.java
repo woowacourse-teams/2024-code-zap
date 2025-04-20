0687ed151eaed909c2e4753fb20e8d4d31a5a144
‘jminkkk’ <102847513+jminkkk@users.noreply.github.com>
2025-04-20 20:31:16 +0900

    refactor(fixture): 별도 사용이 없어진 SourceCode fixture를 제거 및 TemplateFixture 내부에서 SourceCode 생성

--- backend/src/test/java/codezap/fixture/TemplateFixture.java
+++ backend/src/test/java/codezap/fixture/TemplateFixture.java
@@ -1,16 +1,46 @@
 package codezap.fixture;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 import codezap.category.domain.Category;
 import codezap.member.domain.Member;
+import codezap.template.domain.SourceCode;
 import codezap.template.domain.Template;
 import codezap.template.domain.Visibility;
 
 public class TemplateFixture {
     public static Template get(Member member, Category category) {
-        return new Template(member, "안녕", "Description 1", category, Visibility.PUBLIC);
+        return new Template(1L, member, "안녕", "Description 1", category,0L, Visibility.PUBLIC, 0L, getList(1));
     }
 
     public static Template getPrivate(Member member, Category category) {
-        return new Template(member, "안녕하세요", "Description 1", category, Visibility.PRIVATE);
+        return new Template(2L, member, "안녕하세요", "Description 1", category, 0L, Visibility.PRIVATE, 0L, getList(1));
+    }
+
+    public static List<Template> getList(int size, Member member, Category category) {
+        List<Template> templates = new ArrayList<>();
+        IntStream.range(0, size).forEach(i -> templates.add(new Template(
+                (long) 2 + i,
+                member,
+                "title" + i,
+                "description" + i,
+                category,
+                0L,
+                Visibility.PUBLIC,
+                0L,
+                getList(1)
+        )));
+        return templates;
+    }
+
+    private static List<SourceCode> getList(int size) {
+        List<SourceCode> sourceCodes = new ArrayList<>();
+        IntStream.range(0, size).forEach(i -> sourceCodes.add(new SourceCode(
+                "file" + i + ".java",
+                "content" + i
+        )));
+        return sourceCodes;
     }
 }
